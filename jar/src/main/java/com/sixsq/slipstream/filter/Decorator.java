e9d257b0b98cc5a785740e1a0272e7e7aef7ba2d
Marc-Elian Begin <meb@sixsq.com>
2013-07-08 14:42:16 +0200

    Connected reports resource to Enlive

--- jar/src/main/java/com/sixsq/slipstream/filter/Decorator.java
+++ jar/src/main/java/com/sixsq/slipstream/filter/Decorator.java
@@ -22,17 +22,13 @@ package com.sixsq.slipstream.filter;
 
 import org.restlet.Request;
 import org.restlet.Response;
-import org.restlet.data.Cookie;
+import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
 import org.restlet.routing.Filter;
 
-import com.sixsq.slipstream.configuration.Configuration;
-import com.sixsq.slipstream.cookie.CookieUtils;
 import com.sixsq.slipstream.exceptions.SlipStreamException;
-import com.sixsq.slipstream.persistence.User;
-import com.sixsq.slipstream.util.HtmlUtil;
-import com.sixsq.slipstream.util.RequestUtil;
 
 public class Decorator extends Filter {
 
@@ -54,32 +50,18 @@ public class Decorator extends Filter {
 
 	public Representation toHtml(Request request, Response response) throws SlipStreamException {
 
-		String baseUrlSlash = RequestUtil.getBaseUrlSlash(request);
-
-		Configuration configuration = RequestUtil
-				.getConfigurationFromRequest(request);
-
-		Cookie cookie = CookieUtils.extractAuthnCookie(request);
-		String username = CookieUtils.getCookieUsername(cookie);
-
-		User user = User.loadByName(username);
-
 		String xhtmlRepresentation = convertHtmlToXhtml(response.getEntityAsText());
-		
-		String resourceUrl = "run/" + extractRunId(request) + "/reports";
-		
-		return HtmlUtil.transformToHtml(baseUrlSlash, resourceUrl, configuration.version, "reports.xsl", user, xhtmlRepresentation);
 
+		String html = slipstream.ui.views.Representation.toHtml(xhtmlRepresentation,
+				getPageRepresentation(), null);
+		
+		return new StringRepresentation(html, MediaType.TEXT_HTML);
 	}
 
-	private String extractRunId(Request request) {
-		String resourceUri = request.getResourceRef().getPath();
-		int startIndex = "/reports/".length();
-		int endIndex = resourceUri.length()-1;
-		String runId = resourceUri.substring(startIndex, endIndex);
-		return runId;
+	protected String getPageRepresentation() {
+		return "reports";
 	}
-	
+		
 	private String convertHtmlToXhtml(String htmlRepresentation) {
 		return htmlRepresentation.replace("<br>", "<br/>");
 	}
