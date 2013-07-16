75610738f8481b9f5bf7259d385aa62fbf3e4474
Marc-Elian Begin <meb@sixsq.com>
2013-07-16 17:08:51 +0200

    Using ModuleViewPublish class to contain published items

---
+++ jar/src/main/java/com/sixsq/slipstream/module/ModuleViewPublished.java
@@ -0,0 +1,46 @@
+package com.sixsq.slipstream.module;
+
+/*
+ * +=================================================================+
+ * SlipStream Server (WAR)
+ * =====
+ * Copyright (C) 2013 SixSq Sarl (sixsq.com)
+ * =====
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -=================================================================-
+ */
+
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Root;
+
+import com.sixsq.slipstream.persistence.Authz;
+import com.sixsq.slipstream.persistence.ModuleCategory;
+
+@Root(name = "item")
+public class ModuleViewPublished extends ModuleView {
+
+	@Attribute(required = false)
+	private final boolean published;
+
+	public ModuleViewPublished(String resourceUri, String description,
+			ModuleCategory category, String customVersion, Authz authz) {
+
+		super(resourceUri, description, category, customVersion, authz);
+		this.published = true;
+	}
+
+	public boolean isPublished() {
+		return published;
+	}
+
+}
