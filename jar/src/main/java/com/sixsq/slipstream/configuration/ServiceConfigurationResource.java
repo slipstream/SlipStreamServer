slipstream/SlipStreamServer

commit c3d0179fb52dd8d4e8ebe0d21049eba900557b9b

Refactor: unified username extraction from request and html generation

jar/src/main/java/com/sixsq/slipstream/configuration/ServiceConfigurationResource.java
@@ -37,12 +37,20 @@ import com.sixsq.slipstream.exceptions.SlipStreamClientException;
 import com.sixsq.slipstream.exceptions.ValidationException;
 import com.sixsq.slipstream.persistence.ServiceConfiguration;
 import com.sixsq.slipstream.resource.ParameterizedResource;
+import com.sixsq.slipstream.util.RequestUtil;
 
 public class ServiceConfigurationResource extends
 		ParameterizedResource<ServiceConfiguration> {
 
 	public static final String CONFIGURATION_PATH = "/configuration";
 
+	protected Configuration configuration;
+
+	@Override
+	public void doInit() throws ResourceException {
+		super.doInit();
+		configuration = RequestUtil.getConfigurationFromRequest(getRequest());
+	}
 	@Put
 	public void update(Representation entity) {
 
@@ -115,7 +123,7 @@ public class ServiceConfigurationResource extends
 	}
 
 	@Override
-	protected ServiceConfiguration createParameterized(String name)
+	protected ServiceConfiguration getOrCreateParameterized(String name)
 			throws ValidationException {
 		throw (new NotImplementedException());
 	}

jar/src/main/java/com/sixsq/slipstream/application/CommonStatusService.java
@@ -30,7 +30,6 @@ import java.util.List;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.ClientInfo;
-import org.restlet.data.Cookie;
 import org.restlet.data.MediaType;
 import org.restlet.data.Preference;
 import org.restlet.data.Reference;
@@ -42,7 +41,6 @@ import org.restlet.service.StatusService;
 import org.w3c.dom.Document;
 
 import com.sixsq.slipstream.configuration.Configuration;
-import com.sixsq.slipstream.cookie.CookieUtils;
 import com.sixsq.slipstream.exceptions.ConfigurationException;
 import com.sixsq.slipstream.persistence.ServiceConfiguration;
 import com.sixsq.slipstream.persistence.ServiceConfigurationParameter;
@@ -66,8 +64,7 @@ public class CommonStatusService extends StatusService {
 
 		Representation representation = null;
 
-		Cookie cookie = CookieUtils.extractAuthnCookie(request);
-		User user = CookieUtils.getCookieUser(cookie);
+		User user = RequestUtil.getUserFromRequest(request);
 
 		String baseUrlSlash = RequestUtil.getBaseUrlSlash(request);
 

jar/src/main/java/com/sixsq/slipstream/application/RequestViewer.java
@@ -1,93 +1,0 @@
-package com.sixsq.slipstream.application;
-
-/*
- * +=================================================================+
- * SlipStream Server (WAR)
- * =====
- * Copyright (C) 2013 SixSq Sarl (sixsq.com)
- * =====
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * -=================================================================-
- */
-
-import org.restlet.Context;
-import org.restlet.Request;
-import org.restlet.Response;
-import org.restlet.Restlet;
-import org.restlet.data.Cookie;
-import org.restlet.data.MediaType;
-import org.restlet.data.Reference;
-
-import com.sixsq.slipstream.configuration.Configuration;
-import com.sixsq.slipstream.cookie.CookieUtils;
-import com.sixsq.slipstream.util.RequestUtil;
-
-public class RequestViewer extends Restlet {
-
-	private final String name;
-
-	public RequestViewer(String name) {
-		super();
-		this.name = name;
-	}
-
-	public RequestViewer(Context context, String name) {
-		super(context);
-		this.name = name;
-	}
-
-	@Override
-	public void handle(Request request, Response response) {
-
-		String msg = representRequestAsString(request);
-		response.setEntity(msg, MediaType.TEXT_PLAIN);
-	}
-
-	private String representRequestAsString(Request request) {
-
-		StringBuilder msg = new StringBuilder(name + ':' + '\n');
-		msg.append(representRawRequestAsString(request));
-
-		return msg.toString();
-	}
-
-	public static String representRawRequestAsString(Request request) {
-
-		Reference hostRef = request.getHostRef();
-		Reference resourceRef = request.getResourceRef();
-		Reference rootRef = request.getRootRef();
-
-		Configuration configuration = RequestUtil
-				.getConfigurationFromRequest(request);
-
-		StringBuilder msg = new StringBuilder();
-		msg.append("Host URI  : " + hostRef + '\n');
-		msg.append("Res. URI  : " + resourceRef + '\n');
-		msg.append("Root URI  : " + rootRef + '\n');
-		msg.append("Routed    : " + resourceRef.getBaseRef() + '\n');
-		msg.append("Remaining : " + resourceRef.getRemainingPart() + '\n');
-
-		if (configuration != null) {
-			msg.append("Config.   : "
-					+ configuration.getClass().getCanonicalName() + '\n');
-		} else {
-			msg.append("Config.   : NULL\n");
-		}
-
-		Cookie cookie = CookieUtils.extractAuthnCookie(request);
-		msg.append("\n");
-		msg.append(CookieUtils.cookieToString(cookie));
-
-		return msg.toString();
-	}
-}

jar/src/main/java/com/sixsq/slipstream/authn/AuthnResource.java
@@ -21,33 +21,20 @@ package com.sixsq.slipstream.authn;
  */
 
 import org.restlet.Request;
-import org.restlet.data.Cookie;
 import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
-import org.restlet.resource.ResourceException;
-import org.restlet.resource.ServerResource;
 import org.w3c.dom.Document;
 
-import com.sixsq.slipstream.configuration.Configuration;
-import com.sixsq.slipstream.cookie.CookieUtils;
-import com.sixsq.slipstream.persistence.User;
+import com.sixsq.slipstream.resource.BaseResource;
 import com.sixsq.slipstream.util.HtmlUtil;
 import com.sixsq.slipstream.util.RequestUtil;
 import com.sixsq.slipstream.util.SerializationUtil;
 
-public class AuthnResource extends ServerResource {
-
-	protected User user = null;
-
-	protected Configuration configuration = null;
-
-	protected String resourceUri = null;
-
-	protected String baseUrlSlash = null;
+public class AuthnResource extends BaseResource {
 
 	private final String templateName; // name of the page
 
@@ -55,32 +42,16 @@ public class AuthnResource extends ServerResource {
 		this.templateName = templateName;
 	}
 
-	@Override
-	public void doInit() throws ResourceException {
-
-		Request request = getRequest();
-
-		Cookie cookie = CookieUtils.extractAuthnCookie(request);
-		user = CookieUtils.getCookieUser(cookie);
-
-		configuration = RequestUtil.getConfigurationFromRequest(request);
-
-		resourceUri = RequestUtil.extractResourceUri(request);
-
-		baseUrlSlash = RequestUtil.getBaseUrlSlash(request);
-
-	}
-
 	@Get("html")
 	public Representation toHtml() {
 
 		String metadata = "";
-		if (user != null) {
-			Document document = SerializationUtil.toXmlDocument(user);
+		if (getUser() != null) {
+			Document document = SerializationUtil.toXmlDocument(getUser());
 			metadata = SerializationUtil.documentToString(document);
 		}
 		return new StringRepresentation(HtmlUtil.toHtml(metadata, templateName,
-				null, user), MediaType.TEXT_HTML);
+				null, getUser()), MediaType.TEXT_HTML);
 	}
 
 	protected Reference extractRedirectURL(Request request) {

jar/src/main/java/com/sixsq/slipstream/authn/CookieAuthenticator.java
@@ -64,7 +64,6 @@ public class CookieAuthenticator extends Authenticator {
 				CookieUtils.removeAuthnCookie(response);
 			}
 
-			
 			List<MediaType> supported = new ArrayList<MediaType>();
 			supported.add(MediaType.APPLICATION_XML);
 			supported.add(MediaType.TEXT_HTML);

jar/src/main/java/com/sixsq/slipstream/authn/LoginResource.java
@@ -40,6 +40,7 @@ import org.restlet.resource.ResourceException;
 
 import com.sixsq.slipstream.cookie.CookieUtils;
 import com.sixsq.slipstream.persistence.User;
+import com.sixsq.slipstream.util.RequestUtil;
 
 public class LoginResource extends AuthnResource {
 
@@ -47,6 +48,17 @@ public class LoginResource extends AuthnResource {
 
 	public LoginResource() {
 		super("login");
+	}
+
+	@Override
+	protected void doInit() throws ResourceException {
+
+		try {
+			setUser(RequestUtil.getUserFromRequest(getRequest()));
+		} catch (NullPointerException ex) {
+			// user not logged-in. But it's ok for this page
+		}
+
 	}
 
 	@Post

jar/src/main/java/com/sixsq/slipstream/module/ModuleListResource.java
@@ -22,8 +22,6 @@ package com.sixsq.slipstream.module;
 
 import java.util.ArrayList;
 
-import org.restlet.Request;
-import org.restlet.data.Cookie;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
@@ -31,38 +29,21 @@ import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ResourceException;
 
-import com.sixsq.slipstream.configuration.Configuration;
-import com.sixsq.slipstream.cookie.CookieUtils;
-import com.sixsq.slipstream.exceptions.SlipStreamInternalException;
 import com.sixsq.slipstream.module.ModuleView.ModuleViewList;
 import com.sixsq.slipstream.persistence.Module;
-import com.sixsq.slipstream.persistence.User;
+import com.sixsq.slipstream.resource.BaseResource;
 import com.sixsq.slipstream.util.HtmlUtil;
 import com.sixsq.slipstream.util.RequestUtil;
 import com.sixsq.slipstream.util.SerializationUtil;
 
-public class ModuleListResource extends ModuleListResourceBase {
-
-	private Configuration configuration = null;
-
-	protected User user = null;
+public class ModuleListResource extends BaseResource {
 
 	private String resourceUri = null;
 
 	@Override
 	public void doInit() throws ResourceException {
 
-		Request request = getRequest();
-
-		Cookie cookie = CookieUtils.extractAuthnCookie(request);
-		String username = CookieUtils.getCookieUsername(cookie);
-
-		user = User.loadByName(username);
-
-		configuration = RequestUtil.getConfigurationFromRequest(request);
-		if (configuration == null) {
-			throw new SlipStreamInternalException("configuration is null");
-		}
+		super.doInit();
 
 		resourceUri = RequestUtil.extractResourceUri(getRequest());
 
@@ -85,9 +66,8 @@ public class ModuleListResource extends ModuleListResourceBase {
 
 		ModuleViewList moduleViewList = retrieveFilteredModuleViewList();
 
-		return new StringRepresentation(
-				HtmlUtil.toHtml(moduleViewList,
-						getPageRepresentation(), getTransformationType(), user),
+		return new StringRepresentation(HtmlUtil.toHtml(moduleViewList,
+				getPageRepresentation(), getTransformationType(), getUser()),
 				MediaType.TEXT_HTML);
 	}
 
@@ -99,9 +79,10 @@ public class ModuleListResource extends ModuleListResourceBase {
 
 		ModuleViewList modules = new ModuleViewList(
 				Module.viewList(resourceUri));
-		
-		if("/".equals(getRequest().getResourceRef().getPath())) {
-			ModuleViewList published = new ModuleViewList(Module.viewPublishedList());
+
+		if ("/".equals(getRequest().getResourceRef().getPath())) {
+			ModuleViewList published = new ModuleViewList(
+					Module.viewPublishedList());
 			modules.getList().addAll(published.getList());
 		}
 
@@ -112,7 +93,7 @@ public class ModuleListResource extends ModuleListResourceBase {
 		ModuleViewList filtered = new ModuleViewList(
 				new ArrayList<ModuleView>());
 		for (ModuleView view : moduleViewList.getList()) {
-			if (view.getAuthz().canGet(user)) {
+			if (view.getAuthz().canGet(getUser())) {
 				filtered.getList().add(view);
 			}
 		}

jar/src/main/java/com/sixsq/slipstream/module/ModuleResource.java
@@ -309,7 +309,7 @@ public class ModuleResource extends ParameterizedResource<Module> {
 			moduleUri = ModuleUriUtil.extractVersionLessResourceUri(module
 					.getResourceUri());
 			targetUri = ModuleUriUtil
-					.extractVersionLessResourceUri(targetParameterizeUri);
+					.extractVersionLessResourceUri(getTargetParameterizeUri());
 		} catch (ValidationException e) {
 			throwClientValidationError(e.getMessage());
 		}
@@ -471,7 +471,7 @@ public class ModuleResource extends ParameterizedResource<Module> {
 
 	private Module resetMandatoryParameters(Module module)
 			throws ValidationException {
-		for (ModuleParameter referenceParameter : createParameterized(
+		for (ModuleParameter referenceParameter : getOrCreateParameterized(
 				"reference").getParameterList()) {
 			ModuleParameter p = module.getParameter(referenceParameter
 					.getName());
@@ -545,7 +545,7 @@ public class ModuleResource extends ParameterizedResource<Module> {
 			String parentResourceUri = null;
 			try {
 				parentResourceUri = ModuleUriUtil
-						.extractParentUriFromResourceUri(targetParameterizeUri);
+						.extractParentUriFromResourceUri(getTargetParameterizeUri());
 			} catch (ValidationException e) {
 				return false;
 			}
@@ -565,7 +565,7 @@ public class ModuleResource extends ParameterizedResource<Module> {
 	}
 
 	@Override
-	protected Module createParameterized(String name)
+	protected Module getOrCreateParameterized(String name)
 			throws ValidationException {
 		Module module = null;
 		switch (getCategory()) {

commit 56f0aa3ad3bec4e40817e3c330137b4c30fe2a5a

Fixed a bug introduced with the previous commit (c3d0179fb52dd8d4e8ebe0d21049eba900557b9b). Changed the parameter SLIPSTREAM_REGISTRATION_ENABLE to a Boolean.

jar/src/main/java/com/sixsq/slipstream/configuration/ServiceConfigurationResource.java
@@ -48,8 +48,8 @@ public class ServiceConfigurationResource extends
 
 	@Override
 	public void doInit() throws ResourceException {
-		super.doInit();
 		configuration = RequestUtil.getConfigurationFromRequest(getRequest());
+		super.doInit();
 	}
 	@Put
 	public void update(Representation entity) {

jar/src/main/java/com/sixsq/slipstream/persistence/ServiceConfiguration.java
@@ -151,8 +151,8 @@ public class ServiceConfiguration extends
 			}
 		},
 
-		SLIPSTREAM_REGISTRATION_ENABLE("Allow self user registration. If true, user will be able to create accounts themselves.",
-				ParameterCategory.SlipStream_basics);
+		SLIPSTREAM_REGISTRATION_ENABLE("Allow self user registration. If checked, user will be able to create accounts themselves.",
+				ParameterCategory.SlipStream_basics, ParameterType.Boolean);
 
 		private final String description;
 		private final ParameterCategory category;