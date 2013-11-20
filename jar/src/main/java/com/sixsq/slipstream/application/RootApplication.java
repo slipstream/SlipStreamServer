772bb6262a4821a4f1dfc838ffb2aa793ef35cd1
Charles Loomis <cal@sixsq.com>
2013-11-20 15:23:11 +0100

    remove hardcoded examples from code; add examples as files to be uploaded

--- jar/src/main/java/com/sixsq/slipstream/application/RootApplication.java
+++ jar/src/main/java/com/sixsq/slipstream/application/RootApplication.java
@@ -55,8 +55,6 @@ import com.sixsq.slipstream.exceptions.SlipStreamInternalException;
 import com.sixsq.slipstream.exceptions.SlipStreamRuntimeException;
 import com.sixsq.slipstream.exceptions.ValidationException;
 import com.sixsq.slipstream.filter.TrimmedMediaTypesFilter;
-import com.sixsq.slipstream.initialstartup.Images;
-import com.sixsq.slipstream.initialstartup.Tutorials;
 import com.sixsq.slipstream.initialstartup.Users;
 import com.sixsq.slipstream.module.ModuleRouter;
 import com.sixsq.slipstream.persistence.Module;
@@ -114,18 +112,6 @@ public class RootApplication extends Application {
 		} catch (Exception ex) {
 			getLogger().warning(
 					"Error creating default users... already existing?");
-		}
-		try {
-			Images.create();
-		} catch (Exception ex) {
-			getLogger().warning(
-					"Error creating base images... already existing?");
-		}
-		try {
-			Tutorials.create();
-		} catch (Exception ex) {
-			getLogger()
-					.warning("Error creating tutorials... already existing?");
 		}
 	}
 
