16307ac828ea03bb5f630293b911ceaa0807b7dc
zyegfryed <zyegfryedgmail.com>
2013-12-10 15:59:46 +0100

    Fixed metering path.

--- jar/src/main/java/com/sixsq/slipstream/application/RootApplication.java
+++ jar/src/main/java/com/sixsq/slipstream/application/RootApplication.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.application;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -353,11 +353,11 @@ public class RootApplication extends Application {
 		RequestUtil.addConfigurationToRequest(request);
 
 	}
-	
+
 	private void attachMetering(RootRouter router) {
 
 		String hostname = Configuration.getInstance().getProperty(RequiredParameters.SLIPSTREAM_METERING_HOSTNAME.getName());
-		String target = hostname + "/api/v1/meters/{meter}/statistics?{query}";
+		String target = hostname + "/meters/{meter}/statistics?{query}";
 		Redirector redirector = new Redirector(getContext(), target,
 		Redirector.MODE_SERVER_OUTBOUND);
 
