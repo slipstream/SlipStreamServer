0769c80ab5460d5cc009bed572897675dfafb6ee
Charles Loomis <cal@sixsq.com>
2014-05-01 16:55:22 +0200

    add resource validation and startup reconstruction of renewal jobs

--- jar-credcache/src/main/clojure/slipstream/credcache/control.clj
+++ jar-credcache/src/main/clojure/slipstream/credcache/control.clj
@@ -3,7 +3,8 @@
   (:require
     [clojure.tools.logging :as log]
     [clojurewerkz.quartzite.scheduler :as qs]
-    [slipstream.credcache.db-utils :as db]))
+    [slipstream.credcache.db-utils :as db]
+    [slipstream.credcache.credential :as cred]))
 
 (defn- start-quartz
   "Start up the queues for running jobs through the Quartz scheduler."
@@ -32,7 +33,9 @@
     (db/create-client)
     (catch Exception e
       (log/error "error creating Couchbase client:" (.getMessage e))))
-  (start-quartz))
+  (db/add-design-doc)
+  (start-quartz)
+  (cred/reschedule-all-renewals))
 
 (defn stop!
   "Stops the credential management subsystem, freeing the
--- jar-credcache/src/main/clojure/slipstream/credcache/renewal.clj
+++ jar-credcache/src/main/clojure/slipstream/credcache/renewal.clj
@@ -1,25 +1,22 @@
 (ns slipstream.credcache.renewal
-  "Multimethod for renewing a resource that can expire."
+  "Renewable credentials must be renewed periodically to maintain a
+   valid credential.  This namespace defines a multimethod for this
+   renewal.  Each type of renewable credential must implement this
+   method."
   (:require
     [clojure.tools.logging :as log]))
 
-;;
-;; renewable credentials must be renewed periodically to maintain
-;; a valid credential; implementation of such credentials must provide
-;; an implementation of the 'renew' multimethod
-;;
-
 (defmulti renew
           "Renews the credential described in the given map.  This
-           method dispatches on the value of the :typeURI key.  Implementations
-           of this method, must return the updated resource or nil if the
+           method dispatches on the value of the :credential-type key.
+           Implementations must return the updated resource or nil if the
            renewal did not succeed.  The default implementation logs a warning
            and returns nil."
-          :typeURI)
+          :credential-type)
 
 (defmethod renew :default
-           [{:keys [typeURI]}]
-  (log/warn "cannot renew unknown type of credential:" typeURI)
+           [{:keys [credential-type]}]
+  (log/warn "cannot renew unknown type of credential:" credential-type)
   nil)
 
 
