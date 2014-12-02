slipstream/SlipStreamServer

commit 9b9dcdd6d5916ff050571b23ef8f9245b9651949

Improved user validation (fixed #216).

jar-persistence/src/main/java/com/sixsq/slipstream/user/Passwords.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.user;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,8 +23,6 @@ package com.sixsq.slipstream.user;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
-import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
 import com.sixsq.slipstream.persistence.Parameter;
 

jar-connector/src/test/java/com/sixsq/slipstream/util/CommonTestUtil.java
@@ -97,6 +97,10 @@ public abstract class CommonTestUtil {
 			fail();
 		}
 
+		user.setFirstName("Te");
+		user.setLastName("st");
+		user.setEmail("test@example.com");
+
 		try {
 			user.setDefaultCloudServiceName(cloudServiceName);
 		} catch (ValidationException e) {

jar-persistence/src/test/java/com/sixsq/slipstream/user/UserTest.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.user;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -263,11 +263,11 @@ public class UserTest {
 		User user = new User(User.NEW_NAME);
 		user.validate();
 	}
-	
+
 	@Test
 	public void online() {
 		assertThat(user.isOnline(), is(false));
-		
+
 		user.setLastOnline();
 		assertThat(user.isOnline(), is(true));
 
@@ -294,6 +294,11 @@ public class UserTest {
 		} catch (ValidationException e) {
 			e.printStackTrace();
 		}
+
+		user.setFirstName("Te");
+		user.setLastName("st");
+		user.setEmail("test@example.com");
+
 		try {
 			user.hashAndSetPassword(password);
 		} catch (NoSuchAlgorithmException e) {

jar-service/src/main/java/com/sixsq/slipstream/run/Runs.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.run;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,7 +41,6 @@ public class Runs {
 
 	public void populate(User user) throws SlipStreamException {
 		user.validate();
-		User.validateMinimumInfo(user);
 		populateRuns(user, user.isSuper());
 	}
 

jar-service/src/main/java/com/sixsq/slipstream/user/UserResource.java
@@ -39,6 +39,7 @@ import com.sixsq.slipstream.connector.ConnectorFactory;
 import com.sixsq.slipstream.cookie.CookieUtils;
 import com.sixsq.slipstream.exceptions.BadlyFormedElementException;
 import com.sixsq.slipstream.exceptions.ConfigurationException;
+import com.sixsq.slipstream.exceptions.InvalidElementException;
 import com.sixsq.slipstream.exceptions.SlipStreamClientException;
 import com.sixsq.slipstream.exceptions.ValidationException;
 import com.sixsq.slipstream.factory.ParametersFactory;
@@ -301,7 +302,13 @@ public class UserResource extends ParameterizedResource<User> {
 		try {
 			user.validate();
 		} catch (ValidationException ex) {
-			throw new ResourceException(Status.CLIENT_ERROR_CONFLICT, ex);
+			throw new ResourceException(Status.CLIENT_ERROR_CONFLICT, ex.getMessage());
+		}
+
+		try {
+			User.validateMinimumInfo(user);
+		} catch (InvalidElementException ex) {
+			throw new ResourceException(Status.CLIENT_ERROR_CONFLICT, ex.getMessage());
 		}
 
 		if (!getTargetParameterizeUri().equals(user.getName())

jar-service/src/test/java/com/sixsq/slipstream/authn/CookieTest.java
@@ -31,6 +31,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,6 +45,8 @@ import org.restlet.data.Method;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import com.sixsq.slipstream.configuration.Configuration;
 import com.sixsq.slipstream.connector.UserParametersFactoryBase;
@@ -62,8 +66,11 @@ import com.sixsq.slipstream.persistence.UserParameter;
 import com.sixsq.slipstream.run.RunResource;
 import com.sixsq.slipstream.run.RuntimeParameterResource;
 import com.sixsq.slipstream.user.UserResource;
+import com.sixsq.slipstream.user.UserTest;
 import com.sixsq.slipstream.util.CommonTestUtil;
 import com.sixsq.slipstream.util.ResourceTestBase;
+import com.sixsq.slipstream.util.SerializationUtil;
+import com.sixsq.slipstream.util.XmlUtil;
 
 public class CookieTest extends ResourceTestBase {
 
@@ -228,34 +235,35 @@ public class CookieTest extends ResourceTestBase {
 	}
 
 	@Test
-	public void checkStdCookieCanGetSetUserParam()
-			throws ConfigurationException, ValidationException {
+	public void checkStdCookieCanGetSetUserParam() throws ConfigurationException, ValidationException, SAXException,
+			ParserConfigurationException, IOException {
 
 		Response response = getAndPostUser(cookieStd);
 		assertThat(response.getStatus(), is(Status.SUCCESS_ACCEPTED));
 	}
 
 	@Test
-	public void ensureMachineCookieCanotSetUserParam()
-			throws ConfigurationException, ValidationException {
+	public void ensureMachineCookieCanotSetUserParam() throws ConfigurationException, ValidationException,
+			SAXException, ParserConfigurationException, IOException {
 
 		Response response = getAndPostUser(cookieRunA);
 		assertThat(response.getStatus(), is(Status.CLIENT_ERROR_FORBIDDEN));
 	}
 
-	private Response getAndPostUser(Cookie cookie)
-			throws ConfigurationException, ValidationException {
+	private Response getAndPostUser(Cookie cookie) throws ConfigurationException, ValidationException, SAXException,
+			ParserConfigurationException, IOException {
 		Map<String, Object> attributes = new HashMap<String, Object>();
 		attributes.put("user", user.getName());
 
-		Request request = createRequest(attributes, Method.GET, null, null,
-				cookie);
+		Request request = createRequest(attributes, Method.GET, null, null, cookie);
 		request.setClientInfo(getClientInfo());
 		Response response = executeRequest(request, new UserResource());
 
 		assertThat(response.getStatus(), is(Status.SUCCESS_OK));
 
-		String value = response.getEntityAsText();
+		Document XmlDocument = XmlUtil.stringToDom(response.getEntityAsText());
+		XmlDocument.getDocumentElement().setAttribute("password", UserTest.PASSWORD);
+		String value = SerializationUtil.documentToString(XmlDocument);
 
 		Representation entity = new StringRepresentation(value);
 		entity.setMediaType(MediaType.APPLICATION_XML);

jar-service/src/test/java/com/sixsq/slipstream/user/UserResourceTest.java
@@ -302,7 +302,7 @@ public class UserResourceTest extends ResourceTestBase {
 	public void cantSelfAssignSuperUnlessAlreadySuper()
 			throws ConfigurationException, ValidationException {
 
-		User cantSelfAssignSuper = new User("cantSelfAssignSuper");
+		User cantSelfAssignSuper = UserTest.createUser("cantSelfAssignSuper");
 		UserTest.storeUser(cantSelfAssignSuper);
 
 		cantSelfAssignSuper.setSuper(true);
@@ -324,7 +324,7 @@ public class UserResourceTest extends ResourceTestBase {
 	public void superCanAssignSuperToOthers() throws ConfigurationException,
 			ValidationException {
 
-		User superCanAssignSuperToOthers = new User(
+		User superCanAssignSuperToOthers = UserTest.createUser(
 				"superCanAssignSuperToOthers");
 
 		superCanAssignSuperToOthers.setSuper(true);
@@ -408,9 +408,10 @@ public class UserResourceTest extends ResourceTestBase {
 	@Test
 	public void superCreatesInActiveState() throws ValidationException,
 			ConfigurationException {
-		User willBeActive = new User("superCreatesInActiveState");
+		User willBeActive = UserTest.createUser("superCreatesInActiveState");
+		Passwords passwords = new Passwords(null, UserTest.PASSWORD, UserTest.PASSWORD);
 
-		Request request = createPutRequest(willBeActive, superUser.getName());
+		Request request = createPutRequest(willBeActive, superUser.getName(), passwords);
 		Response response = executeRequest(request);
 
 		assertThat(response.getStatus(), is(Status.SUCCESS_CREATED));
@@ -424,9 +425,10 @@ public class UserResourceTest extends ResourceTestBase {
 	@Test
 	public void normalUserCreatedInNewState() throws ValidationException,
 			ConfigurationException {
-		User willBeNew = new User("normalUserCreatedInNewState");
+		User willBeNew = UserTest.createUser("normalUserCreatedInNewState");
+		Passwords passwords = new Passwords(null, UserTest.PASSWORD, UserTest.PASSWORD);
 
-		Request request = createPutRequest(willBeNew, user.getName());
+		Request request = createPutRequest(willBeNew, user.getName(), passwords);
 		Response response = executeRequest(request);
 
 		assertThat(response.getStatus(), is(Status.SUCCESS_CREATED));
@@ -440,10 +442,11 @@ public class UserResourceTest extends ResourceTestBase {
 	@Test
 	public void createWithUserState() throws ValidationException,
 			ConfigurationException {
-		User withState = new User("createWithUserState");
+		User withState = UserTest.createUser("createWithUserState");
 		withState.setState(State.SUSPENDED);
+		Passwords passwords = new Passwords(null, UserTest.PASSWORD, UserTest.PASSWORD);
 
-		Request request = createPutRequest(withState, superUser.getName());
+		Request request = createPutRequest(withState, superUser.getName(), passwords);
 		Response response = executeRequest(request);
 
 		assertThat(response.getStatus(), is(Status.SUCCESS_CREATED));
@@ -457,7 +460,7 @@ public class UserResourceTest extends ResourceTestBase {
 	@Test
 	public void putWithUserState() throws ValidationException,
 			ConfigurationException {
-		User withState = new User("putWithUserState");
+		User withState = UserTest.createUser("putWithUserState");
 		withState = withState.store();
 		withState.setState(State.SUSPENDED);
 
@@ -514,12 +517,19 @@ public class UserResourceTest extends ResourceTestBase {
 			ValidationException {
 		Form form = new Form();
 		form.add("name", targetUser.getName());
-		form.add("firstName", targetUser.getFirstName());
-		form.add("lastName", targetUser.getLastName());
+		form.add("firstname", targetUser.getFirstName());
+		form.add("lastname", targetUser.getLastName());
+		form.add("email", targetUser.getEmail());
 
-		form.add("password1", passwords.newPassword1);
-		form.add("password2", passwords.newPassword2);
-		form.add("oldPassword", passwords.oldPassword);
+		if (passwords.newPassword1 != null) {
+			form.add("password1", passwords.newPassword1);
+		}
+		if (passwords.newPassword2 != null) {
+			form.add("password2", passwords.newPassword2);
+		}
+		if (passwords.oldPassword != null) {
+			form.add("oldPassword", passwords.oldPassword);
+		}
 
 		if (targetUser.isSuper()) {
 			form.add("super", "on");

commit 88e0ef2c9f25c7d4dd708e79e04c799490617a58

Fixed error introduced in 9b9dcdd6d5916ff050571b23ef8f9245b9651949.

jar-persistence/src/main/java/com/sixsq/slipstream/user/Passwords.java
@@ -24,6 +24,8 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
 import com.sixsq.slipstream.persistence.Parameter;
 
 public class Passwords {