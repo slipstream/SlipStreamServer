97aac7a931f94c0945d0f287fc088d930ddb9cae
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2014-12-30 13:30:43 +0100

    Minor.

--- jar-connector/src/main/java/com/sixsq/slipstream/connector/ConnectorBase.java
+++ jar-connector/src/main/java/com/sixsq/slipstream/connector/ConnectorBase.java
@@ -424,8 +424,8 @@ public abstract class ConnectorBase implements Connector {
     }
 
     protected String getParameterValue(String parameterName, ImageModule image) throws ValidationException {
-        ModuleParameter instanceTypeParameter = image.getParameter(constructKey(parameterName));
-        return instanceTypeParameter == null ? null : instanceTypeParameter.getValue();
+        ModuleParameter parameter = image.getParameter(constructKey(parameterName));
+        return parameter == null ? null : parameter.getValue();
     }
 
     protected String getKey(User user) {
