b46dcec2304b938150cce927c55be9ca535e17da
schaubl <lionel@sixsq.com>
2015-02-12 17:11:49 +0100

    Added functions to generate HTML from a JSON string.

--- jar-service/src/main/java/com/sixsq/slipstream/util/HtmlUtil.java
+++ jar-service/src/main/java/com/sixsq/slipstream/util/HtmlUtil.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ import com.sixsq.slipstream.persistence.User;
 
 /**
  * For unit tests, @see HtmlUtilTest
- * 
+ *
  */
 public class HtmlUtil {
 
@@ -62,6 +62,14 @@ public class HtmlUtil {
 		} catch (IllegalArgumentException ex) {
 			throw (new ResourceException(Status.CLIENT_ERROR_NOT_FOUND, "Unknown resource: " + page));
 		}
+	}
+
+	public static String toHtmlFromJson(String json, String page, Request request) {
+		return toHtmlFromJson(json, page, RequestUtil.constructOptions(request));
+	}
+
+	private static String toHtmlFromJson(String json, String page, Map<String, Object> options) {
+		return Representation.toHtml(json, page, options);
 	}
 
 	public static String toHtmlError(String metadata, String error, int code, Request request) {
