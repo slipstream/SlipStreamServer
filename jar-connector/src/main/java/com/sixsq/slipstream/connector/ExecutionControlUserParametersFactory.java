f3e03addd54622e9b0e6250da6bdc382ccb5b4a6
schaubl <lionel@sixsq.com>
2015-02-13 13:19:16 +0100

    Updated descriptions and instructions of the general section of the user profile.

--- jar-connector/src/main/java/com/sixsq/slipstream/connector/ExecutionControlUserParametersFactory.java
+++ jar-connector/src/main/java/com/sixsq/slipstream/connector/ExecutionControlUserParametersFactory.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.connector;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,41 +44,52 @@ public class ExecutionControlUserParametersFactory extends
 	protected void initQuotaParameter()	throws ValidationException {
 		return;
 	}
-	
+
 	@Override
 	protected void initReferenceParameters() throws ValidationException {
 
 		putMandatoryBooleanParameter(
 				UserParameter.KEY_ON_ERROR_RUN_FOREVER,
 				false,
-				"If an error occurs, keep the execution running for investigation.",
+				"If ANY error occurs, keep the execution running.",
+				"This value is useful to investigate the error.",
 				30);
 		putMandatoryBooleanParameter(
 				UserParameter.KEY_ON_SUCCESS_RUN_FOREVER,
 				true,
-				"If no errors occur, keep the execution running. Useful for deployment or long tests.",
+				"If NO errors occur, keep the execution running.",
+				"This value is useful for deployment or long tests.",
 				20);
 		putMandatoryParameter(
 				UserParameter.KEY_TIMEOUT,
-				"Minutes - When this timeout is reached, the execution is forcefully terminated.",
+				"Execution timeout (in minutes).",
 				"30",
+				ParameterType.String,
+				"If the execution stays in a transitional state for more than the value of this timeout, the execution is forcefully terminated.",
 				90);
 		putMandatoryParameter(
 				SSHKEY_PARAMETER_NAME,
-				"SSH Public Key(s) (keys must be separated by new line) Warning: Some clouds may take into account only the first key.",
-				ParameterType.RestrictedText, 40);
+				"SSH Public Key(s) (one per line).",
+				ParameterType.RestrictedText,
+				"Warning: Some clouds may take into account only the first key until SlipStream bootstraps the machine.",
+				40);
+
 		String[] _options = { VERBOSITY_LEVEL_DEFAULT, "1", "2", "3" };
 		List<String> options = Arrays.asList(_options);
 		putMandatoryEnumParameter(
 				VERBOSITY_LEVEL,
-				"Level of verbosity. 0 - Actions, 1 - Steps, 2 - Details data, 3 - Debugging.",
-				options, VERBOSITY_LEVEL_DEFAULT, 99);
+				"Level of verbosity.",
+				options, VERBOSITY_LEVEL_DEFAULT,
+				"0 - Actions,  1 - Steps,  2 - Details data,  3 - Debugging",
+				99);
 
-		List<String> clouds = extractCloudNames(ConnectorFactory
-				.getConnectors());
+		List<String> clouds = extractCloudNames(ConnectorFactory.getConnectors());
 		putMandatoryEnumParameter(
 				UserParametersFactoryBase.DEFAULT_CLOUD_SERVICE_PARAMETER_NAME,
-				"Select which cloud you want to use.", clouds, "", 10);
+				"Select the default cloud.",
+				clouds, "",
+				"Select the cloud that you want to use as the default.",
+				10);
 
 	}
 
