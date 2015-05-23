f3d8e3a02eee215add2731b9d6ef841c1c45b942
Charles Loomis <cal@sixsq.com>
2015-05-23 23:14:21 +0200

    initial implementations for cimi parameter processing

--- ssclj/jar/src/main/clojure/com/sixsq/slipstream/ssclj/middleware/cimi_params.clj
+++ ssclj/jar/src/main/clojure/com/sixsq/slipstream/ssclj/middleware/cimi_params.clj
@@ -1,17 +1,26 @@
 (ns com.sixsq.slipstream.ssclj.middleware.cimi-params
   (:require
     [com.sixsq.slipstream.ssclj.filter.parser :as parser]
-    [clojure.string :as s]))
+    [clojure.string :as s]
+    [instaparse.core :as insta]))
 
 ;;
 ;; List of mime-type abbreviations and the associated full mime-type name.
 ;; All of the mime-type abbreviations must be in lowercase.
 ;;
 (def accepted-mime-types {"json" "application/json",
-                          "xml" "application/xml",
-                          "edn" "application/edn"})
+                          "xml"  "application/xml",
+                          "edn"  "application/edn"})
 
 (def accepted-formats (set (keys accepted-mime-types)))
+
+(defn add-cimi-param
+  "Adds the given key and value to the :cimi-params map in the
+  ring request, creating the map if necessary."
+  [{:keys [cimi-params] :or {:cimi-params {}} :as req} k v]
+  (->> v
+       (assoc cimi-params k)
+       (assoc req :cimi-params)))
 
 (defn as-vector
   "Ensures that the given argument is a vector, coercing the
@@ -46,11 +55,20 @@
        (first)))
 
 (defn get-index
+  "Get the first valid long value for the given named attribute."
   [m k]
   (->> (get m k)
        (first-valid-long)))
 
 (defn process-first-last
+  "Adds the keys :first and :last to the :cimi-params map in the request.
+  If these are not specified or have invalid values, then nil is provided
+  as the value.  Otherwise the value is an integer value corresponding to
+  the attribute value.
+
+  This implementation takes into account the first *valid* value for these
+  attributes, although the specification states that only the first value
+  (valid or otherwise) should be used."
   [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
   (->> ["$first" "$last"]
        (map #(get-index params %))
@@ -58,44 +76,92 @@
        (merge cimi-params)
        (assoc req :cimi-params)))
 
-(defn conjunction
-  "TODO!!"
-  ([m]
-    m)
+(defn filter-conjunction
+  "A reduction function that will combine the given filter(s) with a
+  logical AND expression."
+  ([]
+    [])
   ([a m]
-    m))
+   [:AndExpr a m]))
 
 (defn process-filter
-  [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
+  "Adds the :filter key to the :cimi-params map in the request.  If
+  the $filter parameter appears more than once, then the filters are
+  combined with a logical AND.  Invalid filters are silently ignored."
+
+  [{:keys [params] :or {:params {}} :as req}]
   (->> (get params "$filter")
        (as-vector)
        (map parser/parse-cimi-filter)
-       (reduce conjunction)
-       (assoc cimi-params :filter)
-       (assoc req :cimi-params)))
+       (remove insta/failure?)
+       (map second)
+       (reduce filter-conjunction)
+       (conj [:Filter])
+       (add-cimi-param req :filter)))
 
 (defn comma-split
+  "Split string on commas, optionally surrounded by whitespace.  All values
+  have whitespace trimmed from both ends. Nil values and empty strings are
+  removed from the output.  Passing nil returns an empty vector."
   [s]
-  )
+  (if s
+    (->> (s/split s #"\s*,\s*")
+         (remove nil?)
+         (map s/trim)
+         (remove s/blank?))
+    []))
+
+(defn reduce-expand-set
+  "If the attribute set contains the wildcard '*', then :all is returned
+  Passing nil to the function will return nil."
+  [key-set]
+  (cond
+    (contains? key-set "*") :all
+    (empty? key-set) :none
+    :else key-set))
 
 (defn process-expand
-  [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
+  "Adds the :expand key to the :cimi-params map.  The value will be :none if
+  the attribute wasn't specified or if no valid values were given.  If the
+  wildcard is specified '*', then :all will be given as the value.  In all
+  other cases, a set of the specified attributes will be provided.
+
+  Whitespace around separators and around the attribute names are ignored
+  and removed from the values."
+
+  [{:keys [params] :or {:params {}} :as req}]
   (->> (get params "$expand")
        (as-vector)
        (mapcat comma-split)
        (set)
-       (assoc cimi-params :expand)
-       (assoc req :cimi-params)))
+       (reduce-expand-set)
+       (add-cimi-param req :expand)))
+
+(defn reduce-select-set
+  "If the attribute set contains the wildcard '*', then nil is returned
+  since the effect of the wildcard is the same as if $select was not
+  specified.  Passing nil to the function will also return nil."
+  [key-set]
+  (when-not (contains? key-set "*")
+    key-set))
 
 (defn process-select
-  [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
-  (->> (get params "$select")
-       (as-vector)
-       (mapcat comma-split)
-       (set)
-       (concat #{"resourceURI"})
-       (assoc cimi-params :select)
-       (assoc req :cimi-params)))
+  "Adds the :select key to the :cimi-params map.  The value will be nil if
+  the $select key was not specified or if the wildcard value '*' is given.
+  Otherwise a set of the desired keys is returned with 'resourceURI' added.
+
+  Whitespace surrounding the attribute values is ignored."
+
+  [{:keys [params] :or {:params {}} :as req}]
+  (let [select (get params "$select")
+        v (when select
+            (->> select
+                 (as-vector)
+                 (mapcat comma-split)
+                 (cons "resourceURI")
+                 (set)
+                 (reduce-select-set)))]
+    (add-cimi-param req :select v)))
 
 ;;
 ;;
@@ -108,10 +174,14 @@
     - The first _acceptable_ value is used, rather than strictly the first.
     - Surrounding whitespace is removed from values before processsing.
 
-  In addition to the 'json' and 'xml' values in the specificiation, this
-  also accepts 'edn'."
+  In addition to the 'json' and 'xml' values in the specification, this
+  also accepts 'edn'.
 
-  [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
+  Note that the specification states that this option must override any
+  values provided in the Accept header.  Consequently, the $format value
+  must be checked before generating the output."
+
+  [{:keys [params] :or {:params {}} :as req}]
   (->> (get params "$format")
        (as-vector)
        (filter string?)
@@ -120,28 +190,44 @@
        (filter accepted-formats)
        (map #(get accepted-mime-types %))
        (first)
-       (assoc cimi-params :format)
-       (assoc req :cimi-params)))
+       (add-cimi-param req :format)))
 
-(defn orderby-clause [e]
-  e)
+(defn orderby-clause
+  "Splits an orderby clause value at the colon and then returns a
+  two-value vector with the attribute value and either :asc or :desc.
+  If the attribute value is blank or the value is not valid, then
+  nil will be returned."
+  [s]
+  (let [[attr order] (->> (s/split s #"\s*:\s*")
+                          (take 2)
+                          (remove nil?)
+                          (map s/trim))
+        order (if (.equalsIgnoreCase "desc" order) :desc :asc)]
+    (when-not (s/blank? attr)
+      [attr order])))
 
 (defn process-orderby
-  [{:keys [params cimi-params] :or {:params {} :cimi-params {}} :as req}]
+  "Adds the :orderby parameter to the :cimi-params map in the request.
+  The value of the :orderby key will be an empty list if the parameter
+  isn't specified or has no valid values.  Otherwise it will contain
+  a sequence of attribute name, direction pairs, where the direction is
+  either :asc (ascending) or :desc (descending)."
+
+  [{:keys [params] :or {:params {}} :as req}]
   (->> (get params "$orderby")
        (as-vector)
        (mapcat comma-split)
        (map orderby-clause)
-       (assoc cimi-params :orderby)
-       (assoc req :cimi-params)))
+       (remove nil?)
+       (add-cimi-param req :orderby)))
 
 (defn wrap-cimi-params
   "Middleware that processes CIMI parameters from the :params map
    in the request.  Because this uses the :params map, the wrap-params
    middleware must be run before in the processing chain."
   [handler]
-  (fn [request]
-    (->> (assoc request :cimi-params {})
+  (fn [req]
+    (->> req
          (process-first-last)
          (process-filter)
          (process-expand)
--- ssclj/jar/src/test/clojure/com/sixsq/slipstream/ssclj/middleware/cimi_params_test.clj
+++ ssclj/jar/src/test/clojure/com/sixsq/slipstream/ssclj/middleware/cimi_params_test.clj
@@ -3,6 +3,20 @@
     [com.sixsq.slipstream.ssclj.middleware.cimi-params :refer :all]
     [expectations :refer :all]))
 
+(defn set-and-extract
+  "Sets the $key-name value in the parameters and then extracts
+   the value of :key-name from :cimi-params in the result."
+  [f key-name v]
+  (let [kw (keyword key-name)
+        pname (str "$" key-name)]
+    (->> {:params {pname v}}
+         (f)
+         (:cimi-params)
+         (kw))))
+
+(expect {:cimi-params {:k "value"}} (add-cimi-param {} :k "value"))
+(expect {:cimi-params {:a 1, :k "value"}} (add-cimi-param {:cimi-params {:a 1}} :k "value"))
+
 (expect [] (as-vector nil))
 (expect [1] (as-vector 1))
 (expect ["a"] (as-vector "a"))
@@ -43,24 +57,83 @@
 (expect 1 (get-index {"k" ["a" 1]} "k"))
 (expect 1 (get-index {"k" ["1" "2"]} "k"))
 
+(expect {:cimi-params {:first nil :last nil} :params {}} (process-first-last {:params {}}))
+
 (expect {:first 1 :last 2} (:cimi-params (process-first-last {:params {"$first" 1, "$last" 2}})))
 (expect {:first 1 :last 2} (:cimi-params (process-first-last {:params {"$first" "1", "$last" "2"}})))
 (expect {:first 1 :last 2} (:cimi-params (process-first-last {:params {"$first" ["a" "1"], "$last" ["b" "2"]}})))
 (expect {:first nil :last nil} (:cimi-params (process-first-last {:params {"$first" ["a"], "$last" nil}})))
 (expect {:first nil :last nil} (:cimi-params (process-first-last {:params {}})))
-(expect {:cimi-params {:first nil :last nil} :params {}} (process-first-last {:params {}}))
 
-(expect "application/json" (:format (:cimi-params (process-format {:params {"$format" "json"}}))))
-(expect "application/json" (:format (:cimi-params (process-format {:params {"$format" "JSON"}}))))
-(expect "application/json" (:format (:cimi-params (process-format {:params {"$format" " JSON "}}))))
-(expect "application/xml" (:format (:cimi-params (process-format {:params {"$format" "xml"}}))))
-(expect "application/xml" (:format (:cimi-params (process-format {:params {"$format" "XML"}}))))
-(expect "application/xml" (:format (:cimi-params (process-format {:params {"$format" " XML "}}))))
-(expect "application/edn" (:format (:cimi-params (process-format {:params {"$format" "edn"}}))))
-(expect "application/edn" (:format (:cimi-params (process-format {:params {"$format" "EDN"}}))))
-(expect "application/edn" (:format (:cimi-params (process-format {:params {"$format" " EDN "}}))))
-(expect nil (:format (:cimi-params (process-format {:params {"$format" "unknown"}}))))
+(expect "application/json" (set-and-extract process-format "format" "json"))
+(expect "application/json" (set-and-extract process-format "format" "JSON"))
+(expect "application/json" (set-and-extract process-format "format" " JSON "))
+(expect "application/xml" (set-and-extract process-format "format" "xml"))
+(expect "application/xml" (set-and-extract process-format "format" "XML"))
+(expect "application/xml" (set-and-extract process-format "format" " XML "))
+(expect "application/edn" (set-and-extract process-format "format" "edn"))
+(expect "application/edn" (set-and-extract process-format "format" "EDN"))
+(expect "application/edn" (set-and-extract process-format "format" " EDN "))
 
-(expect "application/json" (:format (:cimi-params (process-format {:params {"$format" ["json" "xml"]}}))))
-(expect "application/json" (:format (:cimi-params (process-format {:params {"$format" ["unknown" "json" "xml"]}}))))
+(expect nil (set-and-extract process-format "format" "unknown"))
+(expect nil (set-and-extract process-format "format" nil))
 
+(expect "application/json" (set-and-extract process-format "format" ["json" "xml"]))
+(expect "application/json" (set-and-extract process-format "format" ["unknown" "json" "xml"]))
+
+(expect ["a" "b"] (comma-split "a,b"))
+(expect ["a" "b"] (comma-split " a , b "))
+(expect ["a" "b"] (comma-split ", a , b ,"))
+(expect [] (comma-split ""))
+(expect [] (comma-split ","))
+(expect [] (comma-split nil))
+
+(expect #{"a" "b"} (reduce-select-set (set ["a" "b" "a" "b"])))
+(expect nil (reduce-select-set (set ["a" "b" "*"])))
+(expect nil (reduce-select-set nil))
+
+(expect nil (set-and-extract process-select "select" nil))
+(expect nil (set-and-extract process-select "select" "*"))
+(expect #{"a" "resourceURI"} (set-and-extract process-select "select" "a"))
+(expect #{"a" "resourceURI"} (set-and-extract process-select "select" " a "))
+(expect #{"a" "resourceURI"} (set-and-extract process-select "select" "a,a"))
+(expect #{"a" "resourceURI"} (set-and-extract process-select "select" [" a,a" "a" "a"]))
+(expect #{"a" "a2" "resourceURI"} (set-and-extract process-select "select" " a, a2 "))
+
+(expect :all (reduce-expand-set #{"*"}))
+(expect :none (reduce-expand-set #{}))
+(expect :none (reduce-expand-set nil))
+(expect #{"a" "b"} (reduce-expand-set #{"a" "b"}))
+
+(expect :none (set-and-extract process-expand "expand" nil))
+(expect :all (set-and-extract process-expand "expand" "*"))
+(expect #{"a"} (set-and-extract process-expand "expand" "a"))
+(expect #{"a" "b"} (set-and-extract process-expand "expand" "a,b"))
+(expect #{"a" "b"} (set-and-extract process-expand "expand" " a , b "))
+(expect #{"a" "b"} (set-and-extract process-expand "expand" ["a" "b"]))
+
+(expect nil (orderby-clause ":"))
+(expect nil (orderby-clause ":a:desc"))
+(expect ["a" :asc] (orderby-clause "a"))
+(expect ["a" :asc] (orderby-clause "a:"))
+(expect ["a" :desc] (orderby-clause "a:desc"))
+(expect ["a" :asc] (orderby-clause "a:dummy"))
+
+(expect [] (set-and-extract process-orderby "orderby" nil))
+(expect [["a" :asc]] (set-and-extract process-orderby "orderby" "a:asc"))
+(expect [["a" :desc]] (set-and-extract process-orderby "orderby" "a:desc"))
+(expect [["a" :desc] ["b" :asc]] (set-and-extract process-orderby "orderby" "a:desc,b"))
+(expect [["a" :desc] ["b" :desc]] (set-and-extract process-orderby "orderby" ["a:desc" "b:desc"]))
+(expect [["a" :desc]] (set-and-extract process-orderby "orderby" ["a:desc" ":b"]))
+(expect [["a" :desc] ["b" :desc]] (set-and-extract process-orderby "orderby" [" a : desc " "b : desc"]))
+
+(expect [:Filter
+         [:AndExpr
+          [:Comp [:Attribute "a"] [:Op "="] [:IntValue "1"]]]]
+        (set-and-extract process-filter "filter" "a=1"))
+
+(expect [:Filter
+         [:AndExpr
+          [:AndExpr [:Comp [:Attribute "a"] [:Op "="] [:IntValue "1"]]]
+          [:AndExpr [:Comp [:Attribute "b"] [:Op "="] [:IntValue "2"]]]]]
+        (set-and-extract process-filter "filter" ["a=1" "b=2"]))
