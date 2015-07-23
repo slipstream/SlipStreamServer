844c199d14bcfa409f970ef39227cab5c9e9fd1a
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2015-07-23 09:56:34 +0200

    Initial commit.

--- jar-persistence/src/main/java/com/sixsq/slipstream/persistence/User.java
+++ jar-persistence/src/main/java/com/sixsq/slipstream/persistence/User.java
@@ -57,11 +57,11 @@ import com.sixsq.slipstream.user.UserView;
  *
  */
 @SuppressWarnings("serial")
-@Entity(name="User")
+@Entity(name="User_")
 @NamedQueries({
-		@NamedQuery(name = "allUsers", query = "SELECT u FROM User u"),
-		@NamedQuery(name = "activeUsers", query = "SELECT u FROM User u WHERE u.state = 'ACTIVE'"),
-		@NamedQuery(name = "userViewList", query = "SELECT NEW com.sixsq.slipstream.user.UserView(u.name, u.firstName, u.lastName, u.email, u.state, u.lastOnline, u.lastExecute, u.activeSince, u.organization, u.isSuperUser) FROM User u") })
+		@NamedQuery(name = "allUsers", query = "SELECT u FROM User_ u"),
+		@NamedQuery(name = "activeUsers", query = "SELECT u FROM User_ u WHERE u.state = 'ACTIVE'"),
+		@NamedQuery(name = "userViewList", query = "SELECT NEW com.sixsq.slipstream.user.UserView(u.name, u.firstName, u.lastName, u.email, u.state, u.lastOnline, u.lastExecute, u.activeSince, u.organization, u.isSuperUser) FROM User_ u") })
 public class User extends Parameterized<User, UserParameter> {
 
 	public static final String REQUEST_KEY = "authenticated_user";
