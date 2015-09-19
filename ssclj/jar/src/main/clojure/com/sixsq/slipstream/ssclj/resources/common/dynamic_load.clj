fcef7ef4c9ffc5129f93737a99ce5e74c250867b
Charles Loomis <cal@sixsq.com>
2015-09-19 17:18:19 +0200

    refactor utilities

--- ssclj/jar/src/main/clojure/com/sixsq/slipstream/ssclj/resources/common/dynamic_load.clj
+++ ssclj/jar/src/main/clojure/com/sixsq/slipstream/ssclj/resources/common/dynamic_load.clj
@@ -2,29 +2,8 @@
   "Utilities for loading information from CIMI resources dynamically."
   (:require
     [clojure.tools.logging                                    :as log]
-    [clojure.java.classpath                                   :as cp]
-    [clojure.tools.namespace.find                             :as nsf]
+    [com.sixsq.slipstream.ssclj.util.namespace-utils          :as dyn]
     [com.sixsq.slipstream.ssclj.resources.common.debug-utils  :as du]))
-
-(defn filter-namespaces
-  "Returns symbols for all of the namespaces that match the given filter
-   function."
-  [f]
-  (->> (cp/classpath)
-       (nsf/find-namespaces)
-       (filter f)))
-
-(defn load-namespace
-  "Dynamically loads the given namespace, returning the namespace.
-   Will return nil if the namespace could not be loaded."
-  [ns-sym]
-  (try
-    (require ns-sym)
-    (log/info "loaded namespace:" ns-sym)
-    (find-ns ns-sym)
-    (catch Exception _
-      (log/error "could not load namespace:" ns-sym)
-      nil)))
 
 (defn resource?
   "If the given symbol represents a resource namespace, the symbol
@@ -38,22 +17,10 @@
       (not (.contains ns-name "test"))
       sym)))
 
-(defn resources
-  "Returns symbols for all of the resource namespaces on the classpath."
+(defn resource-namespaces
+  "Returns a sequence of the resource namespaces on the classpath."
   []
-  (filter-namespaces resource?))
-
-(defn get-ns-var
-  "Retrieves the named var in the given namespace, returning
-   nil if the var could not be found.  Function logs the success or
-   failure of the request."
-  [varname resource-ns]
-
-  (let [v (ns-resolve resource-ns (symbol varname))]
-    (if v
-      (log/debug varname "found in" (ns-name resource-ns))
-      (log/debug varname "NOT found in" (ns-name resource-ns)))
-    v))
+  (dyn/load-filtered-namespaces resource?))
 
 (defn get-resource-link
   "Returns a vector with the resource tag keyword and map with the
@@ -61,14 +28,14 @@
    Function returns nil if either value cannot be found for the
    resource."
   [resource-ns]
-  (if-let [vtag (get-ns-var "resource-tag" resource-ns)]
-    (if-let [vtype (get-ns-var "resource-url" resource-ns)]
+  (if-let [vtag (dyn/get-ns-var "resource-tag" resource-ns)]
+    (if-let [vtype (dyn/get-ns-var "resource-url" resource-ns)]
       [(deref vtag) {:href (deref vtype)}])))
 
 (defn- initialize-resource
   "Run a resource's initialization function if it exists."
   [resource-ns]
-  (if-let [fvar (get-ns-var "initialize" resource-ns)]
+  (if-let [fvar (dyn/get-ns-var "initialize" resource-ns)]
     (try
       ((deref fvar))
       (log/info "initialized resource" (ns-name resource-ns))
@@ -79,10 +46,8 @@
   "Returns a lazy sequence of all of the routes for resources
    discovered on the classpath."
   []
-  (->> (resources)
-       (map load-namespace)
-       (remove nil?)
-       (map (partial get-ns-var "routes"))
+  (->> (resource-namespaces)
+       (map (partial dyn/get-ns-var "routes"))
        (remove nil?)
        (map deref)))
 
@@ -90,9 +55,7 @@
   "Returns a lazy sequence of all of the resource links for resources
    discovered on the classpath."
   []
-  (->> (resources)
-       (map load-namespace)
-       (remove nil?)
+  (->> (resource-namespaces)
        (map get-resource-link)
        (remove nil?)))
 
@@ -100,7 +63,5 @@
   "Runs the initialize function for all resources that define it."
   []
   (doall
-    (->> (resources)
-         (map load-namespace)
-         (remove nil?)
+    (->> (resource-namespaces)
          (map initialize-resource))))
---
+++ ssclj/jar/src/main/clojure/com/sixsq/slipstream/ssclj/util/namespace-utils.clj
@@ -0,0 +1,47 @@
+(ns com.sixsq.slipstream.ssclj.util.namespace-utils
+  "Utilities for dynamic loading of namespaces and vars."
+  (:require
+    [clojure.tools.logging :as log]
+    [clojure.java.classpath :as cp]
+    [clojure.tools.namespace.find :as nsf]))
+
+(defn filter-namespaces
+  "Returns symbols for all of the namespaces that match the given filter
+   function."
+  [f]
+  (->> (cp/classpath)
+       (nsf/find-namespaces)
+       (filter f)))
+
+(defn load-namespace
+  "Dynamically loads the given namespace, returning the namespace.
+   Will return nil if the namespace could not be loaded."
+  [ns-sym]
+  (try
+    (require ns-sym)
+    (log/info "loaded namespace:" ns-sym)
+    (find-ns ns-sym)
+    (catch Exception _
+      (log/error "could not load namespace:" ns-sym)
+      nil)))
+
+(defn load-filtered-namespaces
+  "Returns a sequence of the requested namespaces on the classpath."
+  [f]
+  (->> f
+       (filter-namespaces)
+       (map load-namespace)
+       (remove nil?)))
+
+(defn get-ns-var
+  "Retrieves the named var in the given namespace, returning
+   nil if the var could not be found.  Function logs the success or
+   failure of the request."
+  [varname resource-ns]
+
+  (let [v (ns-resolve resource-ns (symbol varname))]
+    (if v
+      (log/debug varname "found in" (ns-name resource-ns))
+      (log/debug varname "NOT found in" (ns-name resource-ns)))
+    v))
+
