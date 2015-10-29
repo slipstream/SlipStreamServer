49de485ef3ac6272394830e120977bb52c21de97
Stephane Tavera <stephane.tavera@gmail.com>
2015-10-29 11:23:00 +0100

    Remove password in log

---
+++ ssclj/jar/src/test/clojure/com/sixsq/slipstream/ssclj/middleware/logger_test.clj
@@ -0,0 +1,13 @@
+(ns com.sixsq.slipstream.ssclj.middleware.logger-test
+  (require
+    [clojure.test :refer :all]
+    [com.sixsq.slipstream.ssclj.middleware.logger :refer :all]))
+
+(deftest log-does-not-display-password
+  (is (=  "GET auth/login [super ADMIN] ?a=1&b=2 abcdef"
+          (request-to-str
+            { :request-method "GET"
+              :uri "auth/login"
+              :headers {"slipstream-authn-info" "super ADMIN"}
+              :query-string "a=1&password=secret&b=2"
+              :body "abcdef"}))))
