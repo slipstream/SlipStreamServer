3cda51e62beb7fa098822c9d5dc1780d8fe62a00
Stephane Tavera <stephane.tavera@gmail.com>
2016-02-23 12:06:17 +0100

    align versions in leiningen with maven

--- auth/project.clj
+++ auth/project.clj
@@ -1,4 +1,4 @@
-(defproject com.sixsq.slipstream/auth "2.22-SNAPSHOT"
+(defproject com.sixsq.slipstream/auth "2.23.2-SNAPSHOT"
   :description  "Authentication Service"
   :url          "http://sixsq.com"
 
