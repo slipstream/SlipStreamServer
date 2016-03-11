bb879ba00b12d140a848d351b858cd344490bfe7
Stephane Tavera <stephane.tavera@gmail.com>
2016-03-11 18:37:32 +0100

    Add scripts and crons for cloud consolidation

---
+++ ssclj/rpm/src/main/scripts/create-cloud-daily-usage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+java -Dconfig.path=db.spec \
+     -cp "/opt/slipstream/ssclj/resources:/opt/slipstream/ssclj/lib/ext/*:/opt/slipstream/ssclj/lib/ssclj.jar" \
+     com.sixsq.slipstream.ssclj.usage.summarizer -f daily -g cloud
