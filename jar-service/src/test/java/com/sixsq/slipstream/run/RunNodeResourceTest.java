7bc1a359d848e2877b2317c1707bd4c15b996301
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-03-30 09:30:03 +0200

    minor.

--- jar-service/src/test/java/com/sixsq/slipstream/run/RunNodeResourceTest.java
+++ jar-service/src/test/java/com/sixsq/slipstream/run/RunNodeResourceTest.java
@@ -4,7 +4,7 @@ package com.sixsq.slipstream.run;
  * +=================================================================+
  * SlipStream Server (WAR)
  * =====
- * Copyright (C) 2013 SixSq Sarl (sixsq.com)
+ * Copyright (C) 2016 SixSq Sarl (sixsq.com)
  * =====
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
