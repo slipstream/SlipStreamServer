a114f63644b757af89d1c38f445da8f3d2adb33e
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-08-24 23:56:59 +0200

    modified example configs to pass the schema checks; changed unit-testing from expectations to clojure.test.

--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/configuration_schema_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/configuration_schema_test.clj
@@ -1,9 +1,9 @@
 (ns com.sixsq.slipstream.ssclj.resources.configuration-schema-test
   (:require
+    [clojure.test :refer :all]
     [com.sixsq.slipstream.ssclj.resources.configuration :refer :all]
     [com.sixsq.slipstream.ssclj.resources.configuration-template-slipstream :as tpl]
     [schema.core :as s]
-    [expectations :refer :all]
     [com.sixsq.slipstream.ssclj.app.params :as p]))
 
 (def valid-acl {:owner {:principal "::ADMIN"
@@ -12,15 +12,16 @@
                          :principal "ADMIN",
                          :right     "ALL"}]})
 
-(let [timestamp "1964-08-25T10:00:00.0Z"
-      cfg (merge tpl/resource
-                 {:id          (str resource-name "/slipstream")
-                  :resourceURI resource-uri
-                  :created     timestamp
-                  :updated     timestamp
-                  :acl         valid-acl})]
-
-  (expect nil? (s/check Configuration cfg))
-  (expect (s/check Configuration (dissoc cfg :created)))
-  (expect (s/check Configuration (dissoc cfg :updated)))
-  (expect (s/check Configuration (dissoc cfg :acl))))
+(deftest test-configuration-schema-check
+  (let [timestamp "1964-08-25T10:00:00.0Z"
+        cfg {:id          (str resource-name "/slipstream")
+             :resourceURI resource-uri
+             :created     timestamp
+             :updated     timestamp
+             :acl         valid-acl
+             :service     "foo"}
+        check (s/checker Configuration)]
+    (is (nil? (check cfg)))
+    (is (check (dissoc cfg :created)))
+    (is (check (dissoc cfg :updated)))
+    (is (check (dissoc cfg :acl)))))
--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/configuration_template_schema_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/configuration_template_schema_test.clj
@@ -1,8 +1,8 @@
 (ns com.sixsq.slipstream.ssclj.resources.configuration-template-schema-test
   (:require
+    [clojure.test :refer :all]
     [com.sixsq.slipstream.ssclj.resources.configuration-template :refer :all]
     [schema.core :as s]
-    [expectations :refer :all]
     [com.sixsq.slipstream.ssclj.app.params :as p]))
 
 (def valid-acl {:owner {:principal "::ADMIN"
@@ -11,16 +11,16 @@
                          :type      "ROLE"
                          :right     "VIEW"}]})
 
-(let [timestamp "1964-08-25T10:00:00.0Z"
+(deftest test-configuration-template-schema-check
+  (let [timestamp "1964-08-25T10:00:00.0Z"
       root {:id          resource-name
             :resourceURI p/service-context
             :created     timestamp
             :updated     timestamp
             :acl         valid-acl
-            :service     "CloudSoftwareSolution"}]
-
-  (expect nil? (s/check ConfigurationTemplate root))
-  (expect (s/check ConfigurationTemplate (dissoc root :created)))
-  (expect (s/check ConfigurationTemplate (dissoc root :updated)))
-  (expect (s/check ConfigurationTemplate (dissoc root :acl)))
-  (expect (s/check ConfigurationTemplate (dissoc root :cloudServiceType))))
+            :service     "CloudSoftwareSolution"}
+     check (s/checker ConfigurationTemplate)]
+    (is (nil? (check root)))
+    (is (check (dissoc root :created)))
+    (is (check (dissoc root :updated)))
+    (is (check (dissoc root :acl)))))
