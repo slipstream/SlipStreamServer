a8b80692bc4ab87e8dc579e34a02faf3f78e3c3f
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-09-27 12:59:12 +0200

    boot based scripts to simplify testing of ss-conf* CLIs.

---
+++ ssclj/tools/scripts/ssconfig-boot.clj
@@ -0,0 +1,12 @@
+#!/usr/bin/env boot
+
+;; Should be run from root of the project with build.boot.
+
+(load-file "build.boot")
+
+(require '[com.sixsq.slipstream.tools.cli.ssconfig :as d])
+
+(defn -main
+  [& args]
+  (d/-main args))
+
