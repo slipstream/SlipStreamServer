444c4f72b80876f5c8a9df88d273ce4aab517a71
Charles Loomis <cal@sixsq.com>
2017-03-02 08:58:07 +0100

    fix static analysis issues

--- db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
+++ db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
@@ -4,7 +4,7 @@
     [com.sixsq.slipstream.db.es.es-util :as eu]))
 
 (deftest test-create-test-es-client
-  (is (not (= nil (eu/create-test-es-client)))))
+  (is (not (nil? (eu/create-test-es-client)))))
 
 
 
