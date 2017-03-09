b3983075c1802a8b14380a55c2f2eecbbb1ed679
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-03-09 13:10:01 +0100

    protect keys locally.

--- ssclj/rpm/src/main/scripts/generate-auth-keys.sh
+++ ssclj/rpm/src/main/scripts/generate-auth-keys.sh
@@ -3,10 +3,16 @@
 KEYS_LOC=/etc/slipstream/auth
 PRIVKEY=$KEYS_LOC/auth_privkey.pem
 PUBKEY=$KEYS_LOC/auth_pubkey.pem
+user=slipstream
+group=$user
 
 if [ ! -f $PRIVKEY ]; then
    rm -f $PRIVKEY $PUBKEY
    mkdir -p $KEYS_LOC
+   chown $user.$group $KEYS_LOC
+   chmod 700 $KEYS_LOC
    openssl genrsa -out $PRIVKEY 2048
    openssl rsa -pubout -in $PRIVKEY -out $PUBKEY
+   chown $user.$group $PRIVKEY $PUBKEY
+   chmod 400 $PRIVKEY $PRIVKEY
 fi
