9b6c10a55aa1f767ad3c73d3cddc00b6e6699c95
Charles Loomis <cal@sixsq.com>
2017-03-20 11:49:08 +0100

    move some tests from expectations to clojure.test

--- ssclj/jar/test/com/sixsq/slipstream/ssclj/app/main_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/app/main_test.clj
@@ -1,13 +1,15 @@
 (ns com.sixsq.slipstream.ssclj.app.main-test
   (:require
-    [expectations :refer :all]
-    [com.sixsq.slipstream.ssclj.app.main :refer :all]))
+    [clojure.test :refer :all]
+    [com.sixsq.slipstream.ssclj.app.main :as t]))
 
-(expect nil (parse-port nil))
-(expect nil (parse-port (System/getProperties)))
-(expect nil (parse-port "badport"))
-(expect nil (parse-port "-1"))
-(expect nil (parse-port "65536"))
-(expect nil (parse-port "1.3"))
-(expect 1 (parse-port "1"))
-(expect 65535 (parse-port "65535"))
+(deftest check-parse-port
+  (are [expected arg] (is (= expected (t/parse-port arg)))
+                      nil nil
+                      nil (System/getProperties)
+                      nil "badport"
+                      nil "-1"
+                      nil "65536"
+                      nil "1.3"
+                      1 "1"
+                      65535 "65535"))
