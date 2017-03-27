16c17291d3f9aae422edf0c3c4590bea72c24f15
Charles Loomis <cal@sixsq.com>
2017-03-27 10:21:11 +0200

    remove expectations

--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/common/authz_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/common/authz_test.clj
@@ -1,108 +1,109 @@
 (ns com.sixsq.slipstream.ssclj.resources.common.authz-test
   (:require
+    [clojure.test :refer [deftest are is]]
     [com.sixsq.slipstream.ssclj.resources.common.authz :as authz :refer :all]
-    [schema.core :as s]
-    [expectations :refer :all]))
+    [schema.core :as s]))
 
-(expect nil? (current-authentication {}))
-(expect nil? (current-authentication {:identity {:authentications {}}}))
-(expect nil? (current-authentication {:identity {:authentications {"user" {:identity "user"}}}}))
-(expect nil? (current-authentication {:identity {:current         "other"
-                                                 :authentications {"user" {:identity "user"}}}}))
-(expect "user" (:identity (current-authentication {:identity {:current         "user"
-                                                              :authentications {"user" {:identity "user"}}}})))
+(deftest check-current-authentication
+  (are [expect arg] (= expect (:identity (current-authentication arg)))
+                    nil {}
+                    nil {:identity {:authentications {}}}
+                    nil {:identity {:authentications {"user" {:identity "user"}}}}
+                    nil {:identity {:current         "other"
+                                    :authentications {"user" {:identity "user"}}}}
+                    "user" {:identity {:current         "user"
+                                       :authentications {"user" {:identity "user"}}}}))
 
+(deftest check-extract-right
 
-;; Role ADMIN can do anything
-(expect ::authz/all (extract-right {:identity "anyone" :roles ["R1", "ADMIN"]}
-                                   {:type "USER" :principal "USER1" :right "ALL"}))
+  (is (= ::authz/all (extract-right {:identity "anyone" :roles ["R1", "ADMIN"]}
+                                    {:type "USER" :principal "USER1" :right "ALL"})))
 
-(let [id-map {:identity "USER1" :roles ["R1" "R3"]}]
+  (is (= ::authz/view (extract-right nil {:type "ROLE" :principal "ANON" :right "VIEW"})))
+  (is (= ::authz/view (extract-right {} {:type "ROLE" :principal "ANON" :right "VIEW"})))
 
-  (expect ::authz/all (extract-right id-map {:type "USER" :principal "USER1" :right "ALL"}))
+  (let [id-map {:identity "USER1" :roles ["R1" "R3"]}]
 
-  (expect nil? (extract-right id-map {:type "USER" :principal "USER1"}))
-  (expect nil? (extract-right id-map {:type "ROLE" :principal "USER1" :right "ALL"}))
-  (expect ::authz/view (extract-right id-map {:type "ROLE" :principal "R1" :right "VIEW"}))
-  (expect nil? (extract-right id-map {:type "USER" :principal "R1" :right "VIEW"}))
-  (expect nil? (extract-right id-map {:type "ROLE" :principal "R2" :right "MODIFY"}))
-  (expect ::authz/modify (extract-right id-map {:type "ROLE" :principal "R3" :right "MODIFY"}))
-  (expect nil? (extract-right id-map {:type "ROLE" :principal "R3"}))
+    (are [expect arg] (= expect (extract-right id-map arg))
+                      ::authz/all {:type "USER" :principal "USER1" :right "ALL"}
+                      nil {:type "USER" :principal "USER1"}
+                      nil {:type "ROLE" :principal "USER1" :right "ALL"}
+                      ::authz/view {:type "ROLE" :principal "R1" :right "VIEW"}
+                      nil {:type "USER" :principal "R1" :right "VIEW"}
+                      nil {:type "ROLE" :principal "R2" :right "MODIFY"}
+                      ::authz/modify {:type "ROLE" :principal "R3" :right "MODIFY"}
+                      nil {:type "ROLE" :principal "R3"}
+                      nil {:type "ROLE" :principal "ANON"}
+                      ::authz/view {:type "ROLE" :principal "ANON" :right "VIEW"}
+                      nil nil
+                      nil {}))
 
-  (expect nil? (extract-right id-map {:type "ROLE" :principal "ANON"}))
-  (expect ::authz/view (extract-right id-map {:type "ROLE" :principal "ANON" :right "VIEW"}))
-  (expect nil? (extract-right id-map nil))
-  (expect nil? (extract-right id-map {})))
+  (let [acl {:owner {:principal "USER1"
+                     :type      "USER"}
+             :rules [{:principal "ROLE1"
+                      :type      "ROLE"
+                      :right     "VIEW"}
+                     {:principal "USER2"
+                      :type      "USER"
+                      :right     "MODIFY"}]}]
 
-;;
-;; anonymous access does *not* requires authentication
-;;
-(expect ::authz/view (extract-right nil {:type "ROLE" :principal "ANON" :right "VIEW"}))
-(expect ::authz/view (extract-right {} {:type "ROLE" :principal "ANON" :right "VIEW"}))
+    (are [expect arg] (= expect (extract-rights arg acl))
+                      #{} nil
+                      #{} {:identity nil}
+                      #{::authz/all} {:identity "USER1"}
+                      #{::authz/all ::authz/view} {:identity "USER1" :roles ["ROLE1"]}
+                      #{} {:identity "USER_UNKNOWN" :roles ["ROLE_UNKNOWN"]}
+                      #{::authz/view} {:identity "USER_UNKNOWN" :roles ["ROLE1"]}
+                      #{::authz/view ::authz/modify} {:identity "USER2" :roles ["ROLE1"]})))
 
-(let [acl {:owner {:principal "USER1"
-                   :type      "USER"}
-           :rules [{:principal "ROLE1"
-                    :type      "ROLE"
-                    :right     "VIEW"}
-                   {:principal "USER2"
-                    :type      "USER"
-                    :right     "MODIFY"}]}]
+(deftest check-hierarchy
+  (are [parent child] (isa? parent child)
+                      ::authz/all ::authz/view
+                      ::authz/modify ::authz/view
+                      ::authz/modify ::authz/view)
+  (are [parent child] (not (isa? parent child))
+                      ::authz/view ::authz/all
+                      ::authz/view ::authz/modify
+                      ::authz/modify ::authz/all))
 
-  (expect #{} (extract-rights nil acl))
+(deftest check-can-do?
+  (let [acl {:owner {:principal "USER1"
+                     :type      "USER"}
+             :rules [{:principal "ROLE1"
+                      :type      "ROLE"
+                      :right     "VIEW"}
+                     {:principal "USER2"
+                      :type      "USER"
+                      :right     "MODIFY"}]}
+        resource {:acl         acl
+                  :resource-id "Resource/uuid"}]
 
-  (expect #{} (extract-rights {:identity nil} acl))
-  (expect #{::authz/all} (extract-rights {:identity "USER1"} acl))
-  (expect #{::authz/all ::authz/view} (extract-rights {:identity "USER1" :roles ["ROLE1"]} acl))
-  (expect #{} (extract-rights {:identity "USER_UNKNOWN" :roles ["ROLE_UNKNOWN"]} acl))
-  (expect #{::authz/view} (extract-rights {:identity "USER_UNKNOWN" :roles ["ROLE1"]} acl))
-  (expect #{::authz/view ::authz/modify} (extract-rights {:identity "USER2" :roles ["ROLE1"]} acl)))
+    (let [request {:identity {:current         "USER1"
+                              :authentications {"USER1" {:identity "USER1"}}}}]
 
-(expect (isa? ::authz/all ::authz/view))
-(expect (isa? ::authz/modify ::authz/view))
-(expect (isa? ::authz/view ::authz/view))
+      (is (= resource (can-do? resource request ::authz/all)))
+      (is (= resource (can-do? resource request ::authz/modify)))
+      (is (= resource (can-do? resource request ::authz/view)))
 
-(expect false (isa? ::authz/view ::authz/all))
-(expect false (isa? ::authz/view ::authz/modify))
-(expect false (isa? ::authz/modify ::authz/all))
+      (let [request {:identity {:current         "USER_UNKNOWN"
+                                :authentications {"USER_UNKNOWN" {:identity "USER_UNKNOWN"
+                                                                  :roles    ["ROLE1"]}}}}]
 
-(let [acl      {:owner {:principal "USER1"
-                        :type      "USER"}
-                :rules [{:principal "ROLE1"
-                         :type      "ROLE"
-                         :right     "VIEW"}
-                        {:principal "USER2"
-                         :type      "USER"
-                         :right     "MODIFY"}]}
-      resource {:acl         acl
-                :resource-id "Resource/uuid"}]
+        (is (thrown? Exception (can-do? resource request ::authz/all)))
+        (is (thrown? Exception (can-do? resource request ::authz/modify)))
+        (is (= resource (can-do? resource request ::authz/view))))
 
-  (let [request {:identity {:current         "USER1"
-                            :authentications {"USER1" {:identity "USER1"}}}}]
+      (let [request {:identity {:current         "USER2"
+                                :authentications {"USER2" {:identity "USER2"}}}}]
 
-    (expect resource (can-do? resource request ::authz/all))
-    (expect resource (can-do? resource request ::authz/modify))
-    (expect resource (can-do? resource request ::authz/view)))
+        (is (thrown? Exception (can-do? resource request ::authz/all)))
+        (is (= resource (can-do? resource request ::authz/modify)))
+        (is (= resource (can-do? resource request ::authz/view))))
 
-  (let [request {:identity {:current         "USER_UNKNOWN"
-                            :authentications {"USER_UNKNOWN" {:identity "USER_UNKNOWN"
-                                                              :roles    ["ROLE1"]}}}}]
+      (let [request {:identity {:current         "USER2"
+                                :authentications {"USER2" {:identity "USER2"
+                                                           :roles    ["ROLE1"]}}}}]
 
-    (expect Exception (can-do? resource request ::authz/all))
-    (expect Exception (can-do? resource request ::authz/modify))
-    (expect resource (can-do? resource request ::authz/view)))
-
-  (let [request {:identity {:current         "USER2"
-                            :authentications {"USER2" {:identity "USER2"}}}}]
-
-    (expect Exception (can-do? resource request ::authz/all))
-    (expect resource (can-do? resource request ::authz/modify))
-    (expect resource (can-do? resource request ::authz/view)))
-
-  (let [request {:identity {:current         "USER2"
-                            :authentications {"USER2" {:identity "USER2"
-                                                       :roles    ["ROLE1"]}}}}]
-
-    (expect Exception (can-do? resource request ::authz/all))
-    (expect resource (can-do? resource request ::authz/modify))
-    (expect resource (can-do? resource request ::authz/view))))
+        (is (thrown? Exception (can-do? resource request ::authz/all)))
+        (is (= resource (can-do? resource request ::authz/modify)))
+        (is (= resource (can-do? resource request ::authz/view)))))))
