d00fe881e89f091f8cecb75e648fe871e8d38776
Charles Loomis <cal@sixsq.com>
2017-04-16 15:50:11 +0200

    correct unit tests

--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/spec/connector_template_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/spec/connector_template_test.clj
@@ -4,7 +4,11 @@
     [clojure.spec :as s]
 
     [com.sixsq.slipstream.ssclj.app.params :as p]
-    [com.sixsq.slipstream.ssclj.resources.connector-template :as ct]))
+    [com.sixsq.slipstream.ssclj.resources.connector-template :as ct]
+    [com.sixsq.slipstream.ssclj.resources.spec.connector-template :as cts]
+    [com.sixsq.slipstream.ssclj.util.spec :as su]))
+
+(s/def :cimi.test/connector-template (su/only-keys-maps cts/resource-keys-spec))
 
 (def valid-acl {:owner {:principal "ADMIN"
                         :type      "ROLE"}
@@ -19,11 +23,11 @@
                    :created             timestamp
                    :updated             timestamp
                    :acl                 valid-acl
-                   :cloudServiceType    "CloudSoftwareSolution"
+                   :cloudServiceType    "cloud-software-solution"
                    :orchestratorImageid "123"
                    :quotaVm             "20"
                    :maxIaasWorkers      5
                    :instanceName        "foo"}]
-    (is (s/valid? :cimi/connector-template root))
+    (is (s/valid? :cimi.test/connector-template root))
     (doseq [k (into #{} (keys (dissoc root :id :resourceURI)))]
-      (is (not (s/valid? :cimi/connector-template (dissoc root k)))))))
+      (is (not (s/valid? :cimi.test/connector-template (dissoc root k)))))))
--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/spec/connector_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/spec/connector_test.clj
@@ -2,7 +2,12 @@
   (:require
     [clojure.test :refer [deftest are is]]
     [clojure.spec :as s]
-    [com.sixsq.slipstream.ssclj.resources.connector :as c]))
+    [com.sixsq.slipstream.ssclj.resources.connector :as c]
+    [com.sixsq.slipstream.ssclj.resources.spec.connector :as cs]
+    [com.sixsq.slipstream.ssclj.resources.spec.connector-template :as cts]
+    [com.sixsq.slipstream.ssclj.util.spec :as su]))
+
+(s/def :cimi.test/connector (su/only-keys-maps cts/resource-keys-spec))
 
 (def valid-acl {:owner {:principal "ADMIN"
                         :type      "ROLE"}
@@ -22,6 +27,6 @@
               :quotaVm             "20"
               :maxIaasWorkers      5
               :instanceName        "foo"}]
-    (is (s/valid? :cimi/connector root))
+    (is (s/valid? :cimi.test/connector root))
     (doseq [k (into #{} (keys (dissoc root :id :resourceURI)))]
-      (is (not (s/valid? :cimi/connector (dissoc root k)))))))
+      (is (not (s/valid? :cimi.test/connector (dissoc root k)))))))
