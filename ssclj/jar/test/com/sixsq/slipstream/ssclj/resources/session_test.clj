0d44d4d38205ba073c05939d3c9a3ca26f6a322b
Charles (Cal) Loomis <cal@sixsq.com>
2017-05-19 09:10:12 +0200

    allow url encoded session (#1039)
    * allow url encoded session
    
    * minor
    
    * minor

---
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/session_test.clj
@@ -0,0 +1,25 @@
+(ns com.sixsq.slipstream.ssclj.resources.session-test
+  (:require
+    [clojure.test :refer :all]
+    [com.sixsq.slipstream.ssclj.resources.session :as t]))
+
+(deftest check-is-content-type?
+  (are [expected-fn input] (expected-fn (t/is-content-type? input))
+                           true? :content-type
+                           true? "content-type"
+                           true? "Content-Type"
+                           true? "CONTENT-TYPE"
+                           true? "CoNtEnT-TyPe"
+                           false? 1234
+                           false? nil))
+
+(deftest check-is-form?
+  (are [expected-fn input] (expected-fn (t/is-form? input))
+                        true? {:content-type t/form-urlencoded}
+                        true? {"content-type" t/form-urlencoded}
+                        false? {:content-type "application/json"}
+                        false? {"content-type" "application/json"}))
+
+(deftest check-convert-form
+  (is (= {:sessionTemplate {:alpha "alpha", :beta "beta"}}
+         (t/convert-form {:alpha "alpha", "beta" "beta"}))))
