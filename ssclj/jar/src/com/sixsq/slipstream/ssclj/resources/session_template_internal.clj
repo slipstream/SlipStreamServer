9288ced8cc1b344626e0632677251cced88a049f
Charles Loomis <cal@sixsq.com>
2017-05-26 21:29:43 +0200

    ensure internal authentication session template exists

--- ssclj/jar/src/com/sixsq/slipstream/ssclj/resources/session_template_internal.clj
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/resources/session_template_internal.clj
@@ -4,9 +4,20 @@
     [com.sixsq.slipstream.ssclj.resources.spec.session-template-internal]
     [com.sixsq.slipstream.ssclj.resources.session-template :as p]
     [com.sixsq.slipstream.ssclj.resources.common.schema :as c]
-    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]))
+    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
+    [clojure.tools.logging :as log]
+    [com.sixsq.slipstream.ssclj.resources.common.crud :as crud]
+    [com.sixsq.slipstream.db.impl :as db]))
 
 (def ^:const authn-method "internal")
+
+(def default-template {:method      authn-method
+                       :methodKey   authn-method
+                       :name        "Internal"
+                       :description "Internal Authentication via Username/Password"
+                       :username    "username"
+                       :password    "password"
+                       :acl         p/resource-acl})
 
 ;;
 ;; description
@@ -29,11 +40,16 @@
                      :order       4}}))
 
 ;;
-;; initialization: register this Session template
+;; initialization: register this Session template and create internal authentication template
 ;;
 (defn initialize
   []
-  (p/register authn-method desc))
+  (p/register authn-method desc)
+  (try
+    (db/add p/resource-name default-template {:user-name "INTERNAL", :user-roles ["ADMIN"]})
+    (log/info "Created session-template/internal resource.")
+    (catch Exception e
+      (log/warn "session-template/internal resource not created; may already exist; message: " (str e)))))
 
 ;;
 ;; multimethods for validation
