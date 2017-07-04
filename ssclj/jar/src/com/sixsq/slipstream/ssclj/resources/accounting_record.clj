10422c5b085ff7da24892279dff0ef7f692bbe6c
Eric Le Goff <elegoff@sixsq.com>
2017-07-04 09:42:34 +0200

    Work in progress, testing lifecycle

---
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/resources/accounting_record.clj
@@ -0,0 +1,92 @@
+(ns com.sixsq.slipstream.ssclj.resources.accounting_record
+  (:require
+    [clojure.spec.alpha :as s]
+    [com.sixsq.slipstream.ssclj.resources.credential-template-username-password :as tpl]
+    [com.sixsq.slipstream.ssclj.resources.common.std-crud :as std-crud]
+    [com.sixsq.slipstream.ssclj.resources.common.schema :as c]
+    [com.sixsq.slipstream.ssclj.resources.common.crud :as crud]
+    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
+    [com.sixsq.slipstream.auth.acl :as a]
+    [com.sixsq.slipstream.db.impl :as db]
+    [ring.util.response :as r]
+    [clojure.tools.logging :as log]
+    [com.sixsq.slipstream.ssclj.util.log :as logu]))
+
+(def ^:const resource-tag :accountingRecords)
+
+(def ^:const resource-name "AccountingRecord")
+
+(def ^:const resource-url (u/de-camelcase resource-name))
+
+(def ^:const collection-name "AccountingRecordCollection")
+
+(def ^:const resource-uri (str c/slipstream-schema-uri resource-name))
+
+(def ^:const collection-uri (str c/slipstream-schema-uri collection-name))
+
+;; only authenticated users can view and create credentials
+(def collection-acl {:owner {:principal "ADMIN"
+                             :type      "ROLE"}
+                     :rules [{:principal "ADMIN"
+                              :type      "ROLE"
+                              :right     "MODIFY"}
+                            ]})
+
+;;
+;; validate the created credential resource
+;; must dispatch on the type because each credential has a different schema
+;;
+(defmulti validate-subtype :type)
+
+(defmethod validate-subtype :default
+  [resource]
+  (logu/log-and-throw-400 (str "unknown AccountingRecord type: '" (:type resource) "'")))
+
+(defmethod crud/validate resource-uri
+  [resource]
+  (validate-subtype resource))
+
+;;
+;; multimethod for ACLs
+;;
+;;FIXME
+(defn create-acl
+  [id]
+  {:owner {:principal "ADMIN"
+           :type      "ROLE"}
+   :rules [{:principal id
+            :type      "USER"
+            :right     "MODIFY"}]})
+
+(defmethod crud/add-acl resource-uri
+  [{:keys [acl] :as resource} request]
+  (if acl
+    resource
+    (let [user-id (:identity (a/current-authentication request))]
+      (assoc resource :acl (create-acl user-id)))))
+
+;;
+;; CRUD operations
+;;
+
+(def add-impl (std-crud/add-fn resource-name collection-acl resource-uri))
+
+(defmethod crud/add resource-name
+  [request]
+  (add-impl request))
+
+(def retrieve-impl (std-crud/retrieve-fn resource-name))
+(defmethod crud/retrieve resource-name
+  [request]
+  (retrieve-impl request))
+
+(def delete-impl (std-crud/delete-fn resource-name))
+(defmethod crud/delete resource-name
+  [request]
+  (delete-impl request))
+
+(def query-impl (std-crud/query-fn resource-name collection-acl collection-uri resource-tag))
+(defmethod crud/query resource-name
+  [request]
+  (query-impl request))
+
