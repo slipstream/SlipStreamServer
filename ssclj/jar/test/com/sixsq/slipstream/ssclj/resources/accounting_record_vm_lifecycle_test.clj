6ecf5c72bd0039edc8220e6436d9f4f7ee2991f7
Eric Le Goff <elegoff@sixsq.com>
2017-07-04 09:42:34 +0200

    Work in progress, testing lifecycle

---
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/accounting_record_vm_lifecycle_test.clj
@@ -0,0 +1,139 @@
+(ns com.sixsq.slipstream.ssclj.resources.accounting-record-vm-lifecycle-test
+  (:require
+    [clojure.test :refer :all]
+    [clojure.data.json :as json]
+    [peridot.core :refer :all]
+    [com.sixsq.slipstream.ssclj.resources.accounting_record :as accounting]
+    [com.sixsq.slipstream.ssclj.resources.lifecycle-test-utils :as ltu]
+    [com.sixsq.slipstream.ssclj.middleware.authn-info-header :refer [authn-info-header]]
+    [com.sixsq.slipstream.auth.internal :as auth-internal]
+    [com.sixsq.slipstream.auth.utils.db :as db]
+    [com.sixsq.slipstream.ssclj.app.params :as p]
+    [com.sixsq.slipstream.ssclj.app.routes :as routes]
+    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
+    [clojure.spec.alpha :as s]))
+
+(use-fixtures :each ltu/with-test-client-fixture)
+
+(def base-uri (str p/service-context (u/de-camelcase accounting/resource-url)))
+
+(defn ring-app []
+  (ltu/make-ring-app (ltu/concat-routes [(routes/get-main-routes)])))
+
+
+(defn strip-unwanted-attrs [m]
+  (let [unwanted #{:id :resourceURI :acl :operations
+                   :created :updated :name :description}]
+    (into {} (remove #(unwanted (first %)) m))))
+
+(deftest lifecycle
+  (let [
+        session-admin (-> (session (ring-app))
+                          (content-type "application/json")
+                          (header authn-info-header "root ADMIN USER ANON"))
+        session-user (-> (session (ring-app))
+                         (content-type "application/json")
+                         (header authn-info-header "jane USER ANON"))
+        session-anon (-> (session (ring-app))
+                         (content-type "application/json")
+                         (header authn-info-header "unknown ANON"))
+
+        ]
+
+    ;; admin user collection query should succeed but be empty (no accounting records created yet)
+    (-> session-admin
+        (request base-uri)
+        (ltu/body->edn)
+        (ltu/is-status 200)
+        (ltu/is-count zero?)
+        (ltu/is-operation-present "add")
+        (ltu/is-operation-absent "delete")
+        (ltu/is-operation-absent "edit"))
+
+    ;; normal user collection query should succeed but be empty (no credentials created yet)
+    (-> session-user
+        (request base-uri)
+        (ltu/body->edn)
+        (ltu/is-status 200)
+        (ltu/is-count zero?)
+        (ltu/is-operation-absent "add")
+        (ltu/is-operation-absent "delete")
+        (ltu/is-operation-absent "edit"))
+
+    ;; anonymous credential collection query should not succeed
+    (-> session-anon
+        (request base-uri)
+        (ltu/body->edn)
+        (ltu/is-status 403))
+
+    ;; create a new credential as administrator; fail without reference
+    #_(-> session-admin
+          (request base-uri
+                   :request-method :post
+                   :body (json/write-str no-href-create))
+          (ltu/body->edn)
+          (ltu/is-status 400))
+
+    ;; anonymous create without template reference fails
+    #_(-> session-anon
+          (request base-uri
+                   :request-method :post
+                   :body (json/write-str no-href-create))
+          (ltu/body->edn)
+          (ltu/is-status 400))
+
+    ;; admin create with invalid template fails
+    #_(-> session-admin
+          (request base-uri
+                   :request-method :post
+                   :body (json/write-str invalid-create))
+          (ltu/body->edn)
+          (ltu/is-status 400))
+
+    ;; create a credential as a normal user
+    #_(let [create-req {:credentialTemplate {:href         href
+                                             :sshPublicKey "some-key-value"}}
+            resp (-> session-user
+                     (request base-uri
+                              :request-method :post
+                              :body (json/write-str create-req))
+                     (ltu/body->edn)
+                     (ltu/is-status 201))
+            id (get-in resp [:response :body :resource-id])
+            uri (-> resp
+                    (ltu/location))
+            abs-uri (str p/service-context (u/de-camelcase uri))]
+
+        ;; admin should be able to see, edit, and delete credential
+        (-> session-admin
+            (request abs-uri)
+            (ltu/body->edn)
+            (ltu/is-status 200)
+            (ltu/is-operation-present "delete")
+            (ltu/is-operation-present "edit"))
+
+        (-> session-user
+            (request abs-uri)
+            (ltu/body->edn)
+            (ltu/is-status 200)
+            (ltu/is-operation-present "delete")
+            (ltu/is-operation-present "edit"))
+
+        (-> session-user
+            (request abs-uri
+                     :request-method :delete)
+            (ltu/body->edn)
+            (ltu/is-status 200)))))
+
+(deftest bad-methods
+  (let [resource-uri (str p/service-context (u/new-resource-id accounting/resource-name))]
+    (doall
+      (for [[uri method] [[base-uri :options]
+                          [base-uri :delete]
+                          [resource-uri :options]
+                          [resource-uri :post]]]
+        (-> (session (ring-app))
+            (request uri
+                     :request-method method
+                     :body (json/write-str {:dummy "value"}))
+            (ltu/is-status 405))))))
