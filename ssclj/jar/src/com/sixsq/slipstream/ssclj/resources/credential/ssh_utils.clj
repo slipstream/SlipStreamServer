648ecf70cea38d5888205862b3d5491f3b3bfd41
Charles Loomis <cal@sixsq.com>
2017-07-27 20:38:16 +0200

    add ssh utilities

---
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/resources/credential/ssh_utils.clj
@@ -0,0 +1,65 @@
+(ns com.sixsq.slipstream.ssclj.resources.credential.ssh-utils
+  (:require
+    [clojure.string :as str])
+  (:import (com.jcraft.jsch JSch KeyPair)
+           (java.io ByteArrayOutputStream)))
+
+(def ^:const default-key-size 1024)
+
+(def ^:const default-key-type :rsa)
+
+(defn- get-public-key [^KeyPair kp]
+  (with-open [baos (ByteArrayOutputStream.)]
+    (let [fingerprint (.getFingerPrint kp)]
+      (.writePublicKey kp baos fingerprint)
+      (.toString baos "UTF-8"))))
+
+(defn- get-private-key [^KeyPair kp]
+  (with-open [baos (ByteArrayOutputStream.)]
+    (.writePrivateKey kp baos)
+    (.toString baos "UTF-8")))
+
+(def key-name-to-index {"rsa" KeyPair/RSA
+                        "dsa" KeyPair/DSA})
+
+(def key-index-to-name {KeyPair/RSA "rsa"
+                        KeyPair/DSA "dsa"})
+
+(defn generate
+  "Generates a new SSH key pair with the given type and length. The type can
+   either be 'rsa' (default) or 'dsa'. The key size will default to 1024 bits.
+   Note that not all cloud providers support DSA keys."
+  ([]
+   (generate :rsa default-key-size))
+  ([type]
+   (generate type default-key-size))
+  ([type size]
+   (let [type-key (-> type name str/lower-case)]
+     (if-let [type-const (key-name-to-index type-key)]
+       (try
+         (let [kp (KeyPair/genKeyPair (JSch.) type-const size)
+               fingerprint (.getFingerPrint kp)]
+           {:type        type
+            :fingerprint fingerprint
+            :public-key  (get-public-key kp)
+            :private-key (get-private-key kp)})
+         (catch Exception e
+           (throw (ex-info (str e) {}))))
+       (throw (ex-info (str "invalid key type: " type-key) {}))))))
+
+(defn load
+  "Loads a public key (validating it in the process) and then returns a map
+   with the public key information. The public-key parameter must be a string
+   representation of the public key in a supported format."
+  [public-key]
+  (let [^bytes bytes (.getBytes public-key "UTF-8")]
+    (try
+      (let [kp (KeyPair/load (JSch.) nil bytes)
+            key-type (.getKeyType kp)]
+        (if-let [type-name (key-index-to-name key-type)]
+          {:type        type-name
+           :fingerprint (.getFingerPrint kp)
+           :public-key  (get-public-key kp)}
+          (throw (ex-info (str "unsupported public key type: " key-type) {}))))
+      (catch Exception e
+        (throw (ex-info "invalid public key" {}))))))
---
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/credential/ssh_utils_test.clj
@@ -0,0 +1,29 @@
+(ns com.sixsq.slipstream.ssclj.resources.credential.ssh-utils-test
+  (:require
+    [clojure.test :refer [is are deftest]]
+    [com.sixsq.slipstream.ssclj.resources.credential.ssh-utils :as t])
+  (:import
+    (clojure.lang ExceptionInfo)))
+
+(deftest check-generate
+  (doseq [type #{"rsa" "dsa"}
+          size #{1024 2048}]
+    (let [kp (t/generate type size)]
+      (is (= type (:type kp)))
+      (is (re-matches #"^[0-9a-f]{2}(:[0-9a-f]{2}){15}$" (:fingerprint kp)))
+      (is (:public-key kp))
+      (is (:private-key kp)))))
+
+(deftest check-generate-failures
+  (is (thrown-with-msg? ExceptionInfo #"invalid key type: unknown" (t/generate :unknown 1024)))
+  (is (thrown-with-msg? ExceptionInfo #".*RSA keys must be at least 512 bits long.*" (t/generate :rsa 0))))
+
+(deftest check-load
+  (doseq [type #{"rsa" "dsa"}
+          size #{1024 2048}]
+    (let [kp (t/generate type size)
+          loaded-kp (t/load (:public-key kp))]
+      (is (= loaded-kp (dissoc kp :private-key))))))
+
+(deftest check-load-failures
+  (is (thrown-with-msg? ExceptionInfo #"invalid public key" (t/load "invalid-ssh-key"))))
