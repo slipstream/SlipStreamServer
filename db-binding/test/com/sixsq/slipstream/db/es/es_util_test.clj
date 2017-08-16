09b3d1da36f214f761ad32d2cd3483960e7fc7a0
Charles Loomis <cal@sixsq.com>
2017-08-16 14:31:35 +0200

    avoid edge cases

--- db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
+++ db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
@@ -1,21 +1,19 @@
 (ns com.sixsq.slipstream.db.es.es-util-test
   (:require
-   [clojure.test :refer :all]
-   [com.sixsq.slipstream.db.es.es-util :as eu]))
+    [clojure.test :refer :all]
+    [com.sixsq.slipstream.db.es.es-util :as eu]))
 
 (deftest test-create-test-es-client
   (let [[client node] (eu/create-test-es-client)]
-    (is (not (nil? client)))
-    (is (not (nil? node)))))
+    (is client)
+    (is node)))
 
 (deftest test-create-test-index
   (let [[client node] (eu/create-test-es-client)
         lowercases (map char (range 97 122))
-        index-name-length (rand-int 100)
+        index-name-length (inc (rand-int 100))              ;; avoid zero length
         index-name (reduce str (take index-name-length (repeatedly #(rand-nth lowercases))))
         res (eu/create-index client index-name)]
     (is (eu/index-exists? client index-name))
-    (is (not (eu/index-exists? client "wrongindex")))
-    )
-)
+    (is (not (eu/index-exists? client "WrongIndex")))))
 
