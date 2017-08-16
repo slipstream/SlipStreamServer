f4465ad084d9bf4ffbd0bc510ff5a97fa99daa22
Eric Le Goff <elegoff@sixsq.com>
2017-08-16 14:32:22 +0200

    Add a unit for index creation in ES (#1157)
    * Add a unit for index creation in ES
    
    * avoid edge cases

--- db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
+++ db-binding/test/com/sixsq/slipstream/db/es/es_util_test.clj
@@ -5,5 +5,15 @@
 
 (deftest test-create-test-es-client
   (let [[client node] (eu/create-test-es-client)]
-    (is (not (nil? client)))
-    (is (not (nil? node)))))
+    (is client)
+    (is node)))
+
+(deftest test-create-test-index
+  (let [[client node] (eu/create-test-es-client)
+        lowercases (map char (range 97 122))
+        index-name-length (inc (rand-int 100))              ;; avoid zero length
+        index-name (reduce str (take index-name-length (repeatedly #(rand-nth lowercases))))
+        res (eu/create-index client index-name)]
+    (is (eu/index-exists? client index-name))
+    (is (not (eu/index-exists? client "WrongIndex")))))
+
