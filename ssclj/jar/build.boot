894957c8306400200fa9ee6d231d248a1a86ae2b
Eric Le Goff <elegoff@sixsq.com>
2017-09-12 18:04:55 +0200

    Use spandex for bulk insertions

--- ssclj/jar/build.boot
+++ ssclj/jar/build.boot
@@ -24,6 +24,7 @@
                  '[[org.clojure/clojure]
 
                    [aleph]
+                   [cc.qbits/spandex]
                    [cheshire] ;; newer version needed for ring-json
                    [compojure]
                    [com.jcraft/jsch]
--- ssclj/jar/src/com/sixsq/slipstream/ssclj/globalstate/inserter_impl.clj
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/globalstate/inserter_impl.clj
@@ -1,18 +1,55 @@
 (ns com.sixsq.slipstream.ssclj.globalstate.inserter-impl
   (:require
-    [com.sixsq.slipstream.db.es.binding :as esb]
-    [com.sixsq.slipstream.db.impl :as db])
+    [clj-time.core :as t]
+    [qbits.spandex :as spandex]
+    [clojure.core.async :as async])
   (:gen-class)
   )
 
-(defn do-insert
-  [& args]
-  (println "inserting")
+
+;;TODO : parse args for the actual ES endpoint
+(def ^:const client (spandex/client {:hosts ["http://127.0.0.1:9200"]}))
+
+;;define the target index for the global state
+(def ^:const index-action {:index {:_index "global-state" :_type "snapshot"}})
+
+
+(defn bulk-insert
+  [snapshots]
+  (let [{:keys [input-ch output-ch]} (spandex/bulk-chan (eval 'client) {:flush-threshold         100
+                                                                        :flush-interval          5000
+                                                                        :max-concurrent-requests 3})
+        bulk-instructions (vec (interleave (repeat index-action) snapshots))]
+
+    (async/put! input-ch bulk-instructions)))
+
+
+(defn use-pagination
+  [page timestamp]
+  (let [read-vms (-> page
+                  :body
+                  :hits
+                  :hits)
+        source-vms (map :_source read-vms)
+        snapshots (map #(assoc % :snapshot-time timestamp) source-vms)]
+    (bulk-insert snapshots)
+    )
   )
+
+
+(defn fetch-global-state
+  "Retrieves all virtual-machine records ."
+  [& args]
+  (async/go
+    (let [
+          timestamp (str (t/now)) ;; common timestamp for all snapshots
+          ch (spandex/scroll-chan (eval 'client) {:url "resources-index/virtual-machine/_search" :body {:query {:match_all {}}}})]
+      (loop []
+        (when-let [page (async/<! ch)]
+          (use-pagination page timestamp)
+          (recur))))))
 
 (defn -main
   "See tests for examples on how to call from clojure REPL"
   [& args]
-  (db/set-impl! (esb/get-instance))
-  (esb/set-client! (esb/create-client))
-  (apply do-insert args))
+  (apply fetch-global-state args))
