4acedd1c22140b893187008a7299222aa74d6e73
Eric Le Goff <elegoff@sixsq.com>
2017-09-18 15:28:48 +0200

    Use a scheduler rather than a cron job

--- ssclj/jar/src/com/sixsq/slipstream/ssclj/globalstate/inserter_impl.clj
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/globalstate/inserter_impl.clj
@@ -2,54 +2,68 @@
   (:require
     [clj-time.core :as t]
     [qbits.spandex :as spandex]
-    [clojure.core.async :as async])
+    [clojure.core.async :as async]
+    [com.sixsq.slipstream.ssclj.globalstate.scheduler :as scheduler])
   (:gen-class)
   )
 
 
-;;TODO : parse args for the actual ES endpoint
-(def ^:const client (spandex/client {:hosts ["http://127.0.0.1:9200"]}))
-
 ;;define the target index for the global state
 (def ^:const index-action {:index {:_index "global-state" :_type "snapshot"}})
-
+;;initial delay
+(def ^:const immediately 0)
+;;task interval
+(def ^:const every-minute (* 60 1000))
+;;global ES query to search every virtual-machine documents
+(def ^:const search-all-virtual-machines-url "resources-index/virtual-machine/_search")
 
 (defn bulk-insert
-  [snapshots]
-  (let [{:keys [input-ch output-ch]} (spandex/bulk-chan (eval 'client) {:flush-threshold         100
-                                                                        :flush-interval          5000
-                                                                        :max-concurrent-requests 3})
+  "Translate the snapshots (current global state) into ES bulk instructions"
+  [client snapshots]
+  (let [{:keys [input-ch output-ch]} (spandex/bulk-chan client {:flush-threshold         100
+                                                                :flush-interval          1000
+                                                                :max-concurrent-requests 3})
         bulk-instructions (vec (interleave (repeat index-action) snapshots))]
-
     (async/put! input-ch bulk-instructions)))
 
 
 (defn use-pagination
-  [page timestamp]
+  "work on a subset of documents returned by the global query search"
+  [client page timestamp]
   (let [read-vms (-> page
                   :body
                   :hits
                   :hits)
         source-vms (map :_source read-vms)
         snapshots (map #(assoc % :snapshot-time timestamp) source-vms)]
-    (bulk-insert snapshots)
+    (bulk-insert client snapshots)
     )
   )
 
 
 (defn fetch-global-state
   "Retrieves all virtual-machine records ."
-  [& args]
+  []
   (async/go
     (let [
           timestamp (str (t/now)) ;; common timestamp for all snapshots
-          ch (spandex/scroll-chan (eval 'client) {:url "resources-index/virtual-machine/_search" :body {:query {:match_all {}}}})]
+          client (spandex/client {:hosts ["http://127.0.0.1:9200"]})
+          ch (spandex/scroll-chan client
+                                  {:url search-all-virtual-machines-url :body {:query {:match_all {}}}})]
       (loop []
         (when-let [page (async/<! ch)]
-          (use-pagination page timestamp)
+          (use-pagination client page timestamp)
           (recur))))))
 
-(defn -main
-  "See tests for examples on how to call from clojure REPL"
-  [& args]
-  (apply fetch-global-state args))
+
+(defn start []
+  (scheduler/periodically #(fetch-global-state) immediately every-minute))
+
+
+(defn stop []
+  (scheduler/shutdown))
+
+(defn -main [& args]
+  (start))
+
+
---
+++ ssclj/jar/src/com/sixsq/slipstream/ssclj/globalstate/scheduler.clj
@@ -0,0 +1,22 @@
+(ns com.sixsq.slipstream.ssclj.globalstate.scheduler
+  (:import (java.util.concurrent  ScheduledThreadPoolExecutor TimeUnit))
+  )
+
+(def ^:private num-threads 1)
+(def ^:private pool (atom nil))
+
+(defn- thread-pool []
+  (swap! pool (fn [p] (or p (ScheduledThreadPoolExecutor. num-threads)))))
+
+(defn periodically
+  "Schedules function f to run every 'delay' milliseconds after a
+  delay of 'initial-delay'."
+  [f initial-delay delay]
+  (.scheduleWithFixedDelay (thread-pool)
+                           f
+                           initial-delay delay TimeUnit/MILLISECONDS))
+
+(defn shutdown
+  "Terminates all periodic tasks."
+  []
+  (swap! pool (fn [p] (when p (.shutdown p)))))
