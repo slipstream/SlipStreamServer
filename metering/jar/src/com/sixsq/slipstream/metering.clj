7e430da430173749cd2eca47cd6ab4730f87c294
Charles (Cal) Loomis <cal@sixsq.com>
2017-09-22 10:58:20 +0200

    close client; refactoring (#1200)

--- metering/jar/src/com/sixsq/slipstream/metering.clj
+++ metering/jar/src/com/sixsq/slipstream/metering.clj
@@ -28,39 +28,53 @@
                                                                 :flush-interval          1000
                                                                 :max-concurrent-requests 3})
         bulk-instructions (vec (interleave (repeat index-action) snapshots))]
-    (log/debug "handling bulk insert instructions for " (count snapshots) " resources")
-    (async/put! input-ch bulk-instructions)))
+    (log/info "starting bulk insert for " (count snapshots) " resources")
+    (async/put! input-ch bulk-instructions)
+    output-ch))
 
 
-(defn use-pagination
+(defn assoc-snapshot-timestamp
   "work on a subset of documents returned by the global query search"
-  [client page timestamp]
-  (let [read-vms (-> page
-                     :body
-                     :hits
-                     :hits)
-        source-vms (map :_source read-vms)
-        snapshots (map #(assoc % :snapshot-time timestamp) source-vms)]
-    (bulk-insert client snapshots)))
+  [timestamp page]
+  (->> page
+       :body
+       :hits
+       :hits
+       (map :_source)
+       (map #(assoc % :snapshot-time timestamp))))
 
+
+(defn handle-results
+  [ch]
+  (future (loop []
+            (async/<!! ch)
+            (recur))))
 
 (defn fetch-global-state
-  "Retrieves all virtual-machine records ."
+  "Retrieves all virtual-machine records."
   [search-url]
   (async/go
-    (let [
-          timestamp (str (t/now))                           ;; common timestamp for all snapshots
+    (let [timestamp (str (t/now))                           ;; common timestamp for all snapshots
           es-host (or (env/env :es-host) "127.0.0.1")
           es-port (or (env/env :es-port) 9200)
-          client (spandex/client {:hosts [(str "http://" es-host ":" es-port)]})
-          ch (spandex/scroll-chan client
-                                  {:url search-url :body {:query {:match_all {}}}})]
-      (log/info "metering snapshot taken at " timestamp " from " search-url)
-      (loop []
-        (when-let [page (async/<! ch)]
-          (use-pagination client page timestamp)
-          (recur))))
-    :finished                                               ;;don't return nil
+          hosts [(str "http://" es-host ":" es-port)]]
+      (with-open [client (spandex/client {:hosts hosts})]
+        (let [ch (spandex/scroll-chan client
+                                      {:url  search-url
+                                       :body {:query {:match_all {}}}})]
+          (try
+            (log/info "starting metering snapshot " timestamp " from " search-url)
+            (loop []
+              (when-let [page (async/<! ch)]
+                (->> page
+                     (assoc-snapshot-timestamp timestamp)
+                     (bulk-insert client)
+                     handle-results)
+                (recur)))
+            (log/info "finished metering snapshot " timestamp " from " search-url)
+            (finally
+              (async/close! ch))))))
+    :finished                                               ;; don't return nil
     ))
 
 (defn handler [request]
--- metering/jar/test/com/sixsq/slipstream/metering_test.clj
+++ metering/jar/test/com/sixsq/slipstream/metering_test.clj
@@ -1,7 +1,11 @@
 (ns com.sixsq.slipstream.metering-test
   (:require
     [clojure.test :refer [deftest is are]]
-    [com.sixsq.slipstream.metering :as t]))
+    [com.sixsq.slipstream.metering :as t]
+    [clj-time.core :as time]
+    [clojure.core.async :as async]
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]))
 
 (deftest check-str->int
   (are [expected arg] (= expected (t/str->int arg))
@@ -14,3 +18,12 @@
                       1000 1000
                       -10 -10))
 
+(deftest check-assoc-snapshot-timestamp
+  (let [doc (-> (io/resource "virtual-machines.json")
+                slurp
+                (json/read-str :key-fn keyword))
+        timestamp (time/now)
+        results (t/assoc-snapshot-timestamp timestamp doc)]
+    (is (= 10 (count results)))
+    (doseq [result results]
+      (is (= timestamp (:snapshot-time result))))))
