807ab9cb5be3ece0082e63755a27a467d689d053
Charles Loomis <cal@sixsq.com>
2017-12-17 21:43:35 +0100

    remove debugging

--- ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/callback_example_lifecycle_test.clj
+++ ssclj/jar/test/com/sixsq/slipstream/ssclj/resources/callback_example_lifecycle_test.clj
@@ -34,7 +34,6 @@
                                                   (request base-uri
                                                            :request-method :post
                                                            :body (json/write-str create-callback-succeeds))
-                                                  (ltu/dump)
                                                   (ltu/body->edn)
                                                   (ltu/is-status 201)
                                                   :response
