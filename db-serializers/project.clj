f19ae68b5a40c57ce724939f2629e83baa419031
khaled basbous <khaled@sixsq.com>
2017-12-23 23:54:24 +0100

    wip

--- db-serializers/project.clj
+++ db-serializers/project.clj
@@ -6,7 +6,7 @@
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
 (defproject
-  com.sixsq.slipstream/SlipStreamDbBinding-jar
+  com.sixsq.slipstream/SlipStreamDbSerializers-jar
   "3.42-SNAPSHOT"
   :license
   {"Apache 2.0" "http://www.apache.org/licenses/LICENSE-2.0.txt"}
@@ -20,31 +20,24 @@
 
   :resource-paths ["resources"]
 
-  :test-paths ["test"]
-
   :pom-location "target/"
 
   :aot :all
 
-  :dependencies
-  [[org.clojure/clojure]
-   [environ]
-   [cheshire]
-   [org.clojure/data.xml]
-   [clj-time]
-   [me.raynes/fs]
-   [org.clojure/data.json]
-   [org.clojure/tools.logging]
-   [org.elasticsearch/elasticsearch]
-   [org.apache.logging.log4j/log4j-core]
-   [org.apache.logging.log4j/log4j-api]
-   [org.apache.logging.log4j/log4j-web]
-   [org.elasticsearch.client/transport]
-   [ring/ring-json]
-   [superstring]
-   [ring/ring-core]]
+  :dependencies [[org.clojure/clojure]
+                 [environ]
+                 [cheshire]
+                 [camel-snake-kebab]
+                 [me.raynes/fs]
+                 [org.clojure/data.xml]
+                 [superstring]
+                 [org.clojure/test.check]
+                 [com.sixsq.slipstream/SlipStreamPersistence]]
 
   :profiles
-  {:test
-   {:dependencies [[sixsq/build-utils "0.1.4"]]}}
+  {:test     {:dependencies   [[sixsq/build-utils "0.1.4"]]
+              :source-paths   ["test"]
+              :resource-paths ["test-resources"]
+              }
+   :provided {:dependencies [[com.sixsq.slipstream/SlipStreamCljResources-jar]]}}
   )
--- dummy-connector/config/project.clj
+++ dummy-connector/config/project.clj
@@ -6,7 +6,7 @@
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
 (defproject
-  com.sixsq.slipstream/SlipStreamDbBinding-jar
+  com.sixsq.slipstream/SlipStreamConnector-Dummy-conf
   "3.42-SNAPSHOT"
   :license
   {"Apache 2.0" "http://www.apache.org/licenses/LICENSE-2.0.txt"}
@@ -24,27 +24,18 @@
 
   :pom-location "target/"
 
-  :aot :all
-
   :dependencies
-  [[org.clojure/clojure]
-   [environ]
-   [cheshire]
-   [org.clojure/data.xml]
-   [clj-time]
-   [me.raynes/fs]
-   [org.clojure/data.json]
-   [org.clojure/tools.logging]
-   [org.elasticsearch/elasticsearch]
-   [org.apache.logging.log4j/log4j-core]
-   [org.apache.logging.log4j/log4j-api]
-   [org.apache.logging.log4j/log4j-web]
-   [org.elasticsearch.client/transport]
-   [ring/ring-json]
-   [superstring]
-   [ring/ring-core]]
+  [[org.clojure/clojure]]
 
   :profiles
   {:test
-   {:dependencies [[sixsq/build-utils "0.1.4"]]}}
+   {:dependencies [[sixsq/build-utils "0.1.4"]
+                   [com.sixsq.slipstream/SlipStreamCljResourcesTests-jar ~+version+
+                    :classifier "tests" :type "test-jar"]
+                   [peridot]
+                   [commons-logging]
+                   [org.clojure/test.check]]}
+   :provided
+   {:dependencies [[superstring]
+                   [com.sixsq.slipstream/SlipStreamCljResources-jar]]}}
   )
