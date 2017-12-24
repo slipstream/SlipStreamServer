a5de1aff94e067022d74ee17fb8e9a2622579caa
khaled basbous <khaled@sixsq.com>
2017-12-24 09:31:16 +0100

    migration of ring-container

--- ring-container/project.clj
+++ ring-container/project.clj
@@ -6,7 +6,7 @@
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
 (defproject
-  com.sixsq.slipstream/SlipStreamDbSerializers-jar
+  com.sixsq.slipstream/slipstream-ring-container
   "3.42-SNAPSHOT"
   :license
   {"Apache 2.0" "http://www.apache.org/licenses/LICENSE-2.0.txt"}
@@ -18,24 +18,22 @@
 
   :source-paths ["src"]
 
+  :resource-paths ["resources"]
+
   :pom-location "target/"
 
-  :aot :all
+  :aot [sixsq.slipstream.server.ring-container]
 
-  :dependencies [[org.clojure/clojure]
-                 [environ]
-                 [cheshire]
-                 [camel-snake-kebab]
-                 [me.raynes/fs]
-                 [org.clojure/data.xml]
-                 [superstring]
-                 [org.clojure/test.check]
-                 [com.sixsq.slipstream/SlipStreamPersistence]]
+  :dependencies
+  [[org.clojure/clojure]
+   [org.clojure/tools.logging]
+   [aleph]
+   [environ]
+   [log4j]
+   [org.slf4j/slf4j-log4j12]]
 
   :profiles
   {:test     {:dependencies   [[sixsq/build-utils "0.1.4"]]
               :source-paths   ["test"]
-              :resource-paths ["test-resources"]
-              }
-   :provided {:dependencies [[com.sixsq.slipstream/SlipStreamCljResources-jar]]}}
+              :resource-paths ["test-resources"]}}
   )
