1d8eb3b767aa564b6fcaed209bf7cec642f8489a
khaled basbous <khaled@sixsq.com>
2017-12-25 10:25:59 +0100

    migrate jar prs service

---
+++ jar-prs-service/project.clj
@@ -0,0 +1,37 @@
+(def +version+ "3.42-SNAPSHOT")
+
+;; FIXME: Provide HTTPS access to Nexus.
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
+
+(defproject
+  com.sixsq.slipstream/SlipStreamPricingService-jar
+  "3.42-SNAPSHOT"
+  :license
+  {"Apache 2.0" "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+
+  :plugins [[lein-parent "0.3.2"]]
+
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.42-SNAPSHOT"]
+                   :inherit [:min-lein-version :managed-dependencies :repositories]}
+
+  :source-paths ["src"]
+
+  :pom-location "target/"
+
+  :aot :all
+
+  :dependencies
+  [[org.clojure/clojure]
+   [org.clojure/tools.nrepl]
+   [com.sixsq.slipstream/SlipStreamPlacementLib-jar]
+   [com.sixsq.slipstream/token]
+   [compojure]
+   [ring/ring-json]
+   [ring/ring-defaults]]
+
+  :profiles {:test {:dependencies   [[sixsq/build-utils "0.1.4"]]
+                    :source-paths   ["test"]
+                    :resource-paths ["test-resources"]}}
+  )
