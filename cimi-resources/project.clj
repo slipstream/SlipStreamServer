1d6faddc81456b4bef294846a229d005a7849424
Charles Loomis <cal@sixsq.com>
2018-01-25 20:32:24 +0100

    placeholders for separating resources

---
+++ cimi-resources/project.clj
@@ -0,0 +1,31 @@
+(def +version+ "3.45-SNAPSHOT")
+
+;; FIXME: Provide HTTPS access to Nexus.
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
+
+(defproject
+  com.sixsq.slipstream/SlipStreamServer-cimi-resources
+  "3.45-SNAPSHOT"
+  :license
+  {"Apache 2.0" "http://www.apache.org/licenses/LICENSE-2.0.txt"}
+
+  :plugins [[lein-parent "0.3.2"]
+            [lein-environ "1.1.0"]]
+
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45-SNAPSHOT"]
+                   :inherit [:min-lein-version
+                             :managed-dependencies
+                             :repositories
+                             :deploy-repositories]}
+
+  :source-paths ["src"]
+
+  :pom-location "target/"
+
+  :dependencies
+  [[org.clojure/clojure]]
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure]]}
+             :test     {:aot :all}})
---
+++ cimi-resources/test/com/sixsq/slipstream/ssclj/resources/placeholder_test.clj
@@ -0,0 +1,7 @@
+(ns com.sixsq.slipstream.ssclj.resources.placeholder-test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest addition
+  (= 3 (+ 1 2)))
+
+
