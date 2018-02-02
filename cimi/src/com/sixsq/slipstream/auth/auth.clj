1765f445b333f08486c49df7f5fa19dfa143f065
Charles Loomis <cal@sixsq.com>
2018-02-02 15:03:20 +0100

    remove unused parts of deprecated authentication

--- cimi/src/com/sixsq/slipstream/auth/auth.clj
+++ cimi/src/com/sixsq/slipstream/auth/auth.clj
@@ -1,12 +1,9 @@
 (ns com.sixsq.slipstream.auth.auth
   (:refer-clojure :exclude [update])
   (:require
-    [clojure.data.json :as json]
-    [clojure.tools.logging :as log]
     [com.sixsq.slipstream.auth.internal :as ia]
-    [com.sixsq.slipstream.auth.github :as gh]
-    [com.sixsq.slipstream.auth.cyclone :as cy]
     [com.sixsq.slipstream.auth.utils.http :as uh]))
+
 
 (defn dispatch-on-authn-method
   [request]
@@ -15,19 +12,14 @@
       keyword
       (or :internal)))
 
+
 (defmulti login dispatch-on-authn-method)
+
 
 (defmethod login :internal
   [request]
   (ia/login request))
 
-(defmethod login :github
-  [_]
-  (gh/login))
-
-(defmethod login :cyclone
-  [_]
-  (cy/login))
 
 (defn logout
   [_]
