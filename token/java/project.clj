007fd5c1320df4724cad99fd3f17ef04ffbbb836
sixsq-hudson <hudson@sixsq.com>
2018-02-04 06:56:12 +0000

    release v3.45

--- token/java/project.clj
+++ token/java/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.45-SNAPSHOT")
+(def +version+ "3.45")
 
-(defproject com.sixsq.slipstream/token-java "3.45-SNAPSHOT"
+(defproject com.sixsq.slipstream/token-java "3.45"
 
   :description "token management utilities for java"
 
@@ -12,7 +12,7 @@
 
   :plugins [[lein-parent "0.3.2"]]
 
-  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45-SNAPSHOT"]
+  :parent-project {:coords  [com.sixsq.slipstream/parent "3.45"]
                    :inherit [:min-lein-version :managed-dependencies :repositories :deploy-repositories]}
 
   :pom-location "target/"
