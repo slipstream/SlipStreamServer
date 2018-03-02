1719a8e876f2914d3c4761ed3f3702ce083f4447
Cristovao Cordeiro <cristovao.cordeiro@sixsq.com>
2018-03-02 07:46:14 +0100

    finish containerization and tests with default es container (#1412)
    * finish containerization and tests with default es container
    
    * formatting changes; change address to host
    
    * further renaming; propagate changes to other modules
    
    * move docker compose yaml to readme instructions

--- ring-container/test/sixsq/slipstream/server/ring_container_test.clj
+++ ring-container/test/sixsq/slipstream/server/ring_container_test.clj
@@ -4,6 +4,7 @@
     [sixsq.slipstream.server.ring-container :as t])
   (:import (clojure.lang ExceptionInfo)))
 
+
 (deftest check-as-symbol
   (let [as-symbol @#'t/as-symbol]
     (are [expected input] (= expected (as-symbol input))
@@ -13,11 +14,13 @@
     (is (thrown-with-msg? ExceptionInfo #"invalid symbol" (as-symbol 10)))
     (is (thrown-with-msg? ExceptionInfo #"invalid symbol" (as-symbol nil)))))
 
+
 (deftest check-ns-and-var
   (let [ns-and-var @#'t/ns-and-var]
     (are [expected input] (= expected (ns-and-var input))
                           '[environ.core env] 'environ.core/env)
     (is (thrown-with-msg? ExceptionInfo #"complete, namespaced" (ns-and-var 'no-namespace)))))
+
 
 (deftest check-resolve-var
   (let [resolve-var @#'t/resolve-var]
@@ -26,6 +29,7 @@
                  '[aleph.http start-server])
     (is (nil? (apply resolve-var '[environ.core unknown])))
     (is (thrown-with-msg? ExceptionInfo #"could not resolve" (apply resolve-var '[unknown unknown])))))
+
 
 (deftest check-dyn-resolve
   (let [dyn-resolve @#'t/dyn-resolve]
@@ -37,6 +41,18 @@
     (is (thrown-with-msg? ExceptionInfo #"complete, namespaced" (dyn-resolve 'invalid.symbol)))
     (is (thrown-with-msg? ExceptionInfo #"error requiring namespace" (dyn-resolve 'unknown/var)))
     (is (thrown-with-msg? ExceptionInfo #"error requiring namespace" (dyn-resolve "unknown/var")))))
+
+
+(deftest check-validate-host
+  (let [validate-host @#'t/validate-host]
+    (are [expected input] (= expected (validate-host input))
+                          t/default-host nil
+                          t/default-host 10.2
+                          t/default-host 65537
+                          t/default-host -1
+                          "10" "10"
+                          "0.0.0.0" "0.0.0.0")))
+
 
 (deftest check-parse-port
   (let [parse-port @#'t/parse-port]
