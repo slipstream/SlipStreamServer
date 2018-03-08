2145d13ab8f559ab41e7f589baed8ccb1bdf1d8c
Charles Loomis <cal@sixsq.com>
2018-03-08 14:25:56 +0100

    update parent to 5.0.1; git push

--- jar-prslib/project.clj
+++ jar-prslib/project.clj
@@ -15,7 +15,7 @@
             [lein-shell "0.5.0"]
             [lein-localrepo "0.5.4"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.0.0"]
+  :parent-project {:coords  [sixsq/slipstream-parent "5.0.1"]
                    :inherit [:min-lein-version
                              :managed-dependencies
                              :repositories
