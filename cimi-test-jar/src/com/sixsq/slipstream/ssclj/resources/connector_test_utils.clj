3bc842bca3015ce60ddbd078f60472fbdc4ef109
Eric Le Goff <elegoff@sixsq.com>
2018-04-25 09:45:53 +0200

    Fix for backward compatibility

--- cimi-test-jar/src/com/sixsq/slipstream/ssclj/resources/connector_test_utils.clj
+++ cimi-test-jar/src/com/sixsq/slipstream/ssclj/resources/connector_test_utils.clj
@@ -21,6 +21,7 @@
   [cloud-service-type]
   (str cloud-service-type "-" (System/currentTimeMillis)))
 
+
 ;;
 ;; Tests.
 ;;
@@ -31,120 +32,120 @@
    (connector-lifecycle cloud-service-type nil))
 
   ([cloud-service-type provided-template]
-  (let [href (str ct/resource-url "/" cloud-service-type)
-        template-url (str p/service-context ct/resource-url "/" cloud-service-type)
+   (let [href (str ct/resource-url "/" cloud-service-type)
+         template-url (str p/service-context ct/resource-url "/" cloud-service-type)
 
-        session-anon (-> (ltu/ring-app)
-                         session
-                         (content-type "application/json"))
-        session-admin (header session-anon authn-info-header "root ADMIN")
+         session-anon (-> (ltu/ring-app)
+                          session
+                          (content-type "application/json"))
+         session-admin (header session-anon authn-info-header "root ADMIN")
 
-        resp (-> session-admin
-                 (request template-url)
+         resp (-> session-admin
+                  (request template-url)
+                  (ltu/body->edn)
+                  (ltu/is-status 200))
+         template (or provided-template (get-in resp [:response :body]))
+         valid-create {:connectorTemplate (-> template
+                                              ltu/strip-unwanted-attrs
+                                              (assoc :instanceName (new-instance-name cloud-service-type)))}
+         href-create {:connectorTemplate (merge provided-template {:href         href
+                                                                   :instanceName (new-instance-name cloud-service-type)})}
+         invalid-create (assoc-in valid-create [:connectorTemplate :invalid] "BAD")]
+
+     ;; admin create with invalid template fails
+     (-> session-admin
+         (request base-uri
+                  :request-method :post
+                  :body (json/write-str invalid-create))
+         (ltu/body->edn)
+         (ltu/is-status 400))
+
+     ;; full connector lifecycle as administrator should work
+     (let [uri (-> session-admin
+                   (request base-uri
+                            :request-method :post
+                            :body (json/write-str valid-create))
+                   (ltu/body->edn)
+                   (ltu/is-status 201)
+                   (ltu/location))
+           abs-uri (str p/service-context (u/de-camelcase uri))]
+
+
+       ;; create again with the same connector instance name should fail with 409
+       (-> session-admin
+           (request base-uri
+                    :request-method :post
+                    :body (json/write-str (assoc valid-create :instanceName uri)))
+           (ltu/body->edn)
+           (ltu/is-status 400))
+
+       ;; admin get succeeds
+       (-> session-admin
+           (request abs-uri)
+           (ltu/body->edn)
+           (ltu/is-status 200))
+
+       ;; anonymous query fails
+       (-> session-anon
+           (request base-uri)
+           (ltu/body->edn)
+           (ltu/is-status 403))
+
+       ;; admin query succeeds
+       (let [entries (-> session-admin
+                         (request base-uri)
+                         (ltu/body->edn)
+                         (ltu/is-status 200)
+                         (ltu/is-resource-uri con/collection-uri)
+                         (ltu/is-count 1)
+                         (ltu/entries con/resource-tag))]
+         (is ((set (map :id entries)) uri))
+
+         ;; verify that all entries are accessible
+         (let [pair-fn (juxt :id #(str p/service-context (:id %)))
+               pairs (map pair-fn entries)]
+           (doseq [[id entry-uri] pairs]
+             (-> session-admin
+                 (request entry-uri)
                  (ltu/body->edn)
-                 (ltu/is-status 200))
-        template (or provided-template (get-in resp [:response :body]))
-        valid-create {:connectorTemplate (-> template
-                                             ltu/strip-unwanted-attrs
-                                             (assoc :instanceName (new-instance-name cloud-service-type)))}
-        href-create {:connectorTemplate {:href         href
-                                         :instanceName (new-instance-name cloud-service-type)}}
-        invalid-create (assoc-in valid-create [:connectorTemplate :invalid] "BAD")]
+                 (ltu/is-status 200)
+                 (ltu/is-id id)))))
 
-    ;; admin create with invalid template fails
-    (-> session-admin
-        (request base-uri
-                 :request-method :post
-                 :body (json/write-str invalid-create))
-        (ltu/body->edn)
-        (ltu/is-status 400))
+       ;; admin delete succeeds
+       (-> session-admin
+           (request abs-uri
+                    :request-method :delete)
+           (ltu/body->edn)
+           (ltu/is-status 200))
 
-    ;; full connector lifecycle as administrator should work
-    (let [uri (-> session-admin
-                  (request base-uri
-                           :request-method :post
-                           :body (json/write-str valid-create))
-                  (ltu/body->edn)
-                  (ltu/is-status 201)
-                  (ltu/location))
-          abs-uri (str p/service-context (u/de-camelcase uri))]
+       ;; ensure entry is really gone
+       (-> session-admin
+           (request abs-uri)
+           (ltu/body->edn)
+           (ltu/is-status 404)))
 
+     ;; abbreviated lifecycle using href to template instead of copy
+     (let [uri (-> session-admin
+                   (request base-uri
+                            :request-method :post
+                            :body (json/write-str href-create))
+                   (ltu/body->edn)
+                   (ltu/is-status 201)
+                   (ltu/location))
+           abs-uri (str p/service-context (u/de-camelcase uri))]
 
-      ;; create again with the same connector instance name should fail with 409
-      (-> session-admin
-          (request base-uri
-                   :request-method :post
-                   :body (json/write-str (assoc valid-create :instanceName uri)))
-          (ltu/body->edn)
-          (ltu/is-status 400))
+       ;; admin delete succeeds
+       (-> session-admin
+           (request abs-uri
+                    :request-method :delete)
+           (ltu/body->edn)
+           (ltu/is-status 200))
 
-      ;; admin get succeeds
-      (-> session-admin
-          (request abs-uri)
-          (ltu/body->edn)
-          (ltu/is-status 200))
-
-      ;; anonymous query fails
-      (-> session-anon
-          (request base-uri)
-          (ltu/body->edn)
-          (ltu/is-status 403))
-
-      ;; admin query succeeds
-      (let [entries (-> session-admin
-                        (request base-uri)
-                        (ltu/body->edn)
-                        (ltu/is-status 200)
-                        (ltu/is-resource-uri con/collection-uri)
-                        (ltu/is-count 1)
-                        (ltu/entries con/resource-tag))]
-        (is ((set (map :id entries)) uri))
-
-        ;; verify that all entries are accessible
-        (let [pair-fn (juxt :id #(str p/service-context (:id %)))
-              pairs (map pair-fn entries)]
-          (doseq [[id entry-uri] pairs]
-            (-> session-admin
-                (request entry-uri)
-                (ltu/body->edn)
-                (ltu/is-status 200)
-                (ltu/is-id id)))))
-
-      ;; admin delete succeeds
-      (-> session-admin
-          (request abs-uri
-                   :request-method :delete)
-          (ltu/body->edn)
-          (ltu/is-status 200))
-
-      ;; ensure entry is really gone
-      (-> session-admin
-          (request abs-uri)
-          (ltu/body->edn)
-          (ltu/is-status 404)))
-
-    ;; abbreviated lifecycle using href to template instead of copy
-    (let [uri (-> session-admin
-                  (request base-uri
-                           :request-method :post
-                           :body (json/write-str href-create))
-                  (ltu/body->edn)
-                  (ltu/is-status 201)
-                  (ltu/location))
-          abs-uri (str p/service-context (u/de-camelcase uri))]
-
-      ;; admin delete succeeds
-      (-> session-admin
-          (request abs-uri
-                   :request-method :delete)
-          (ltu/body->edn)
-          (ltu/is-status 200))
-
-      ;; ensure entry is really gone
-      (-> session-admin
-          (request abs-uri)
-          (ltu/body->edn)
-          (ltu/is-status 404))))))
+       ;; ensure entry is really gone
+       (-> session-admin
+           (request abs-uri)
+           (ltu/body->edn)
+           (ltu/is-status 404))))))
 
 
 (defn connector-template-is-registered
