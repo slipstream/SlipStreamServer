9aac01d1612fd9d9cec67b2e85cede06823b8ef4
Charles Loomis <cal@sixsq.com>
2018-05-04 13:09:27 +0200

    fix dependency; remove unnecessary file

--- db-serializers/project.clj
+++ db-serializers/project.clj
@@ -3,9 +3,9 @@
 (defproject com.sixsq.slipstream/SlipStreamDbSerializers-jar "3.51-SNAPSHOT"
 
   :description "utilities for serializing objects to a database"
-  
+
   :url "https://github.com/slipstream/SlipStreamServer"
-  
+
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"
             :distribution :repo}
@@ -32,6 +32,7 @@
                  [org.clojure/data.xml]
                  [superstring]
                  [org.clojure/test.check]
+                 [com.sixsq.slipstream/SlipStreamDbBinding-jar ~+version+]
                  [com.sixsq.slipstream/SlipStreamDbTesting-jar ~+version+]]
 
   :profiles
