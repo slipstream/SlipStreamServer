5710be8130f156cc38ac14ac3948bc491bd9f725
Charles Loomis <cal@sixsq.com>
2018-05-07 15:34:17 +0200

    fix tests

--- db-serializers/test/com/sixsq/slipstream/db/serializers/utils_test.clj
+++ db-serializers/test/com/sixsq/slipstream/db/serializers/utils_test.clj
@@ -1,19 +1,11 @@
 (ns com.sixsq.slipstream.db.serializers.utils-test
   (:require
-    [clojure.test :refer :all]
-    [com.sixsq.slipstream.db.serializers.utils :as u]
-    [com.sixsq.slipstream.dbtest.es.utils-esdb :as ud]))
+    [clojure.test :refer [deftest is]]
+    [com.sixsq.slipstream.db.serializers.utils :as u]))
 
 (deftest test-as-boolean
-  (is (= true (u/as-boolean true)))
-  (is (= false (u/as-boolean false)))
-  (is (= true (u/as-boolean "true")))
-  (is (= false (u/as-boolean "false")))
-  (is (= nil (u/as-boolean nil))))
-
-(deftest test-start-stop-es
-  (is (instance? clojure.lang.Var$Unbound ud/*es-server*))
-  (ud/create-test-es-db-uncond)
-  (is (not (instance? clojure.lang.Var$Unbound ud/*es-server*)))
-  (ud/close-es-server!)
-  (is (instance? clojure.lang.Var$Unbound ud/*es-server*)))
+  (is (true? (u/as-boolean true)))
+  (is (false? (u/as-boolean false)))
+  (is (true? (u/as-boolean "true")))
+  (is (false? (u/as-boolean "false")))
+  (is (nil? (u/as-boolean nil))))
