606833e55c4c11733a6ee9fd1712dd92d7bee1a3
Charles Loomis <cal@sixsq.com>
2018-06-02 07:26:18 +0200

    remove CYCLONE public key and references

--- cimi/src/com/sixsq/slipstream/auth/oidc.clj
+++ cimi/src/com/sixsq/slipstream/auth/oidc.clj
@@ -1,16 +1,17 @@
 (ns com.sixsq.slipstream.auth.oidc
-  (:require [clj-http.client :as http]
-            [clojure.data.json :as json]))
+  (:require
+    [clj-http.client :as http]
+    [clojure.data.json :as json]))
 
 
 (defn get-oidc-access-token
-[oidc-client-id oidc-base-url oidc-code redirect-uri]
-(-> (http/post (str oidc-base-url "/token")
-               {:headers     {"Accept" "application/json"}
-                :form-params {:grant_type   "authorization_code"
-                              :code         oidc-code
-                              :redirect_uri redirect-uri
-                              :client_id    oidc-client-id}})
-    :body
-    (json/read-str :key-fn keyword)
-    :access_token))
+  [oidc-client-id oidc-base-url oidc-code redirect-uri]
+  (-> (http/post (str oidc-base-url "/token")
+                 {:headers     {"Accept" "application/json"}
+                  :form-params {:grant_type   "authorization_code"
+                                :code         oidc-code
+                                :redirect_uri redirect-uri
+                                :client_id    oidc-client-id}})
+      :body
+      (json/read-str :key-fn keyword)
+      :access_token))
