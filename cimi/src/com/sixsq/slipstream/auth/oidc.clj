0d1dd4ea32477cc0737cc1c76f688da9584a14c8
Charles Loomis <cal@sixsq.com>
2018-07-12 10:41:11 +0200

    add logging for errors when retrieving access token

--- cimi/src/com/sixsq/slipstream/auth/oidc.clj
+++ cimi/src/com/sixsq/slipstream/auth/oidc.clj
@@ -1,18 +1,32 @@
 (ns com.sixsq.slipstream.auth.oidc
   (:require
     [clj-http.client :as http]
-    [clojure.data.json :as json]))
+    [clojure.data.json :as json]
+    [clojure.pprint :refer [pprint]]
+    [clojure.tools.logging :as log]))
 
 
 (defn get-access-token
+  "Perform an HTTP POST to the OIDC/MitreID server to recover an access token.
+   This method will log exceptions but then return nil to indicate that the
+   access token could not be retrieved."
   [client-id client-secret tokenURL oidc-code redirect-uri]
-  (-> (http/post tokenURL
-                 {:headers     {"Accept" "application/json"}
-                  :form-params {:grant_type    "authorization_code"
-                                :code          oidc-code
-                                :redirect_uri  redirect-uri
-                                :client_id     client-id
-                                :client_secret client-secret}})
-      :body
-      (json/read-str :key-fn keyword)
-      :access_token))
+  (try
+    (-> (http/post tokenURL
+                   {:headers     {"Accept" "application/json"}
+                    :form-params {:grant_type    "authorization_code"
+                                  :code          oidc-code
+                                  :redirect_uri  redirect-uri
+                                  :client_id     client-id
+                                  :client_secret client-secret}})
+        :body
+        (json/read-str :key-fn keyword)
+        :access_token)
+    (catch Exception e
+      (let [client-secret? (str (boolean client-secret))]
+        (if-let [{:keys [status body]} (ex-data e)]
+          (log/errorf "error status %s getting access token from %s with client_id %s, code %s, and client_secret %s\n%s"
+                      status tokenURL client-id oidc-code client-secret? (with-out-str (pprint body)))
+          (log/errorf "unexpected error when getting access token from %s with client_id %s, code %s, and client_secret %s\n{}"
+                      tokenURL client-id oidc-code client-secret? (str e))))
+      nil)))
