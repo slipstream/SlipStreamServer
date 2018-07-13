665aa7d930542870757260c298a9d2a145572823
Charles Loomis <cal@sixsq.com>
2018-07-13 09:12:26 +0200

    partial implementation of oidc token authentication

---
+++ cimi/src/com/sixsq/slipstream/ssclj/resources/session_template_oidc_token.clj
@@ -0,0 +1,47 @@
+(ns com.sixsq.slipstream.ssclj.resources.session-template-oidc-token
+  (:require
+    [com.sixsq.slipstream.ssclj.resources.common.std-crud :as std-crud]
+    [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
+    [com.sixsq.slipstream.ssclj.resources.session-template :as p]
+    [com.sixsq.slipstream.ssclj.resources.spec.session-template-oidc-token :as session-tpl]))
+
+
+(def ^:const authn-method "oidc-token")
+
+
+(def default-template {:method      authn-method
+                       :instance    authn-method
+                       :name        "OIDC Token"
+                       :description "Direct Authentication with OIDC Token"
+                       :token       "token"
+                       :acl         p/resource-acl})
+
+;;
+;; description
+;;
+(def ^:const desc
+  (merge p/SessionTemplateDescription
+         {:token {:displayName "OIDC Token"
+                  :category    "general"
+                  :description "OIDC Token"
+                  :type        "string"
+                  :mandatory   true
+                  :readOnly    false
+                  :order       20}}))
+
+;;
+;; initialization: register this Session template
+;;
+(defn initialize
+  []
+  (p/register authn-method desc)
+  (std-crud/initialize p/resource-url ::session-tpl/oidc-token))
+
+;;
+;; multimethods for validation
+;;
+
+(def validate-fn (u/create-spec-validation-fn ::session-tpl/oidc-token))
+(defmethod p/validate-subtype authn-method
+  [resource]
+  (validate-fn resource))
