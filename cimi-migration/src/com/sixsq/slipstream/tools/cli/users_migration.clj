7cc9efee5301b396d3af313809ad37725e3fbb80
Eric Le Goff <elegoff@sixsq.com>
2018-07-13 16:11:56 +0200

    Uncomment resource creation in script and provide README

--- cimi-migration/src/com/sixsq/slipstream/tools/cli/users_migration.clj
+++ cimi-migration/src/com/sixsq/slipstream/tools/cli/users_migration.clj
@@ -7,27 +7,25 @@
     [com.sixsq.slipstream.ssclj.resources.user.user-identifier-utils :as uiu]
     [taoensso.timbre :as log]
     [clojure.string :as str])
-  (:gen-class)
-  )
+  (:gen-class))
 
 (def ^:private active-user-filter "(state='ACTIVE')")
 
 (def default-db-binding-ns "com.sixsq.slipstream.db.es.loader")
 
-(def ^:const hn-orgs #{"SixSq" "RHEA" "CERN" "CNRS" "DESY"  "KIT" "INFN" "IFAE" "EMBL" "SURFSara"})
+(def ^:const hn-orgs #{"SixSq" "RHEA" "CERN" "CNRS" "DESY" "KIT" "INFN" "IFAE" "EMBL" "SURFSara"})
 
 (def hn-usernames-to-ignore #{"super" "sixsq" "sixsq_prod" "julz"
-                              "student01" "student02"  "student03" "student04" "student05"
+                              "student01" "student02" "student03" "student04" "student05"
                               "student06" "student07" "student08" "student09" "student10"
                               "student11" "student12" "student13" "student14" "student15"
                               "student16" "student17" "student18" "student19" "student20"
                               "konstan_cotec" "konstan_gnss" "loomis" "test" "konstan"
                               "cjdcsixsq" "sixsq_dev" "mht" "sixsq_ci" "sebastien.fievet"
-                              "rob" "elegoff" "cjdc"
-                              "meb" "LouMeri" "casdfafasf" "testuser" "lionel" "doug"
+                              "rob" "elegoff" "cjdc" "meb" "LouMeri" "casdfafasf" "testuser"
+                              "lionel" "doug"
                               "m.betti.rhea" "gbol16" "cedricseynat" "hnrp2"
-                              "evamvako" "evamvak" "vamvakop"
-                              })
+                              "evamvako" "evamvak" "vamvakop"})
 
 
 (defn init-db-client
@@ -55,19 +53,17 @@
 (defn split-identifier
   "returns a tuple containing the 2 parts of an identifier"
   [identifier]
-  (let [ [instance login] (str/split identifier #":") ]
+  (let [[instance login] (str/split identifier #":")]
     [instance login]))
 
 
 (defn migrate-users-with-external-identity
-[users]
+  [users]
   (doseq [{:keys [username externalIdentity] :as u} users]
     (let [identifier-tuples (map split-identifier externalIdentity)]
-      (doseq [[inst login]  identifier-tuples  ]
+      (doseq [[inst login] identifier-tuples]
         (log/debugf "Add User Identifier username = %s external-login = %s and instance  = %s" username login inst)
-        #_(uiu/add-user-identifier! username nil external-login instance)))))
-
-
+        (uiu/add-user-identifier! username nil login inst)))))
 
 
 (defn find-users-with-githublogin
@@ -80,12 +76,7 @@
   [users]
   (doseq [{:keys [username githublogin] :as u} users]
     (log/debugf "Add User Identifier username = %s authn-method=github and external-login = %s " username githublogin)
-   #_(uiu/add-user-identifier! username "github" githublogin nil)
-
-    ))
-
-
-
+    (uiu/add-user-identifier! username "github" githublogin nil)))
 
 (defn find-users-by-organization
   [org]
@@ -94,56 +85,63 @@
         matched-users (find-users filter-str)]
     matched-users))
 
+
+(defn mangled-usernames-to-ignore
+  "identify users who appear to be duplicated , "
+  [users]
+  (let [freq (frequencies (->> users (map :username) (map uiu/sanitize-login-name)))]
+    (set (map first (filter #(> (second %) 1) freq)))))
+
+
+
 (defn migrate-hn-users
   [users]
-  (let [test-user (:username (first users))]
-  (doseq [{:keys [ username organization]   } users]
-    (log/debugf "username = %s method oidc and external-login=%s and instance = %s"  username username (str/lower-case organization))
-    #_(when (= username test-user)  (uiu/add-user-identifier! username nil username (str/lower-case organization))
-    ))))
+    (doseq [{:keys [username organization]} users]
+      (log/debugf "username = %s method oidc and external-login=%s and instance = %s" username username (str/lower-case organization))
+       (uiu/add-user-identifier! username nil username (str/lower-case organization))))
 
 (defn migrate-biosphere-users
   [users]
-  (let [test-user (:username (first users))]
-  (doseq [{:keys [ id username ]   } users]
-    (when (not= username (-> id
-                           (str/split #"/")
-                           second
-                           ))
-      (log/warnf "username = %s but user-id = %s" username id))
-    (log/debugf "username = %s method oidc and external-login=%s and instance = biosphere"  username username)
-    #_(when (= username test-user)(uiu/add-user-identifier! username nil username "biosphere")))))
+    (doseq [{:keys [id username]} users]
+      (when (not= username (-> id
+                               (str/split #"/")
+                               second
+                               ))
+        (log/warnf "username = %s but user-id = %s" username id))
+      (log/debugf "username = %s method oidc and external-login=%s and instance = biosphere" username username)
+      (uiu/add-user-identifier! username nil username "biosphere")))
 
-
-(defn filter-non-OIDC-users
+(defn filter-hn-users
   [users]
-  (filter #(-> %
-               :username
-               hn-usernames-to-ignore
-               not) users))
+  (let [blacklisted-usernames (clojure.set/union hn-usernames-to-ignore (mangled-usernames-to-ignore users))]
+    (filter #(-> %
+                 :username
+                 blacklisted-usernames
+                 not) users)))
 
 
-  (defn -main [& args]
-    (let [_ (init-db-client default-db-binding-ns)
-            extIdentity (find-users-with-externalIdentities)
-            githublogin-users (find-users-with-githublogin)
-            hn-users (-> (mapcat find-users-by-organization hn-orgs)
-                         filter-non-OIDC-users)
-            biosphere-users (find-users-by-organization "Biosphere")]
-      (log/info "=== Migrating user identifiers ===")
 
-      (log/debugf "----- %s users are found with an externalIdentity attribute " (count extIdentity))
-      (log/debug  "----------------------------------------------------------------")
-      (migrate-users-with-external-identity extIdentity)
+(defn -main [& args]
+  (let [_ (init-db-client default-db-binding-ns)
+        extIdentity (find-users-with-externalIdentities)
+        githublogin-users (find-users-with-githublogin)
+        hn-users (-> (mapcat find-users-by-organization hn-orgs)
+                     filter-hn-users)
+        biosphere-users (find-users-by-organization "Biosphere")]
+    (log/info "=== Migrating user identifiers ===")
 
-      (log/debugf "----- %s users are found with an githublogin attribute " (count githublogin-users))
-      (log/debug  "----------------------------------------------------------------")
-      (migrate-users-with-githublogin githublogin-users)
+    (log/debugf "----- %s users are found with an externalIdentity attribute " (count extIdentity))
+    (log/debug "----------------------------------------------------------------")
+    (migrate-users-with-external-identity extIdentity)
 
-      (log/debugf "----- %s users are found with a HnSciCloud organization " (count hn-users))
-      (log/debug  "----------------------------------------------------------------")
-      (migrate-hn-users hn-users)
+    (log/debugf "----- %s users are found with an githublogin attribute " (count githublogin-users))
+    (log/debug "----------------------------------------------------------------")
+    (migrate-users-with-githublogin githublogin-users)
 
-      (log/debugf "----- %s users are found with a BioSphere organization " (count biosphere-users))
-      (log/debug  "----------------------------------------------------------------")
-      (migrate-biosphere-users biosphere-users)))
+    (log/debugf "----- %s users are found with a HnSciCloud organization " (count hn-users))
+    (log/debug "----------------------------------------------------------------")
+    (migrate-hn-users hn-users)
+
+    (log/debugf "----- %s users are found with a BioSphere organization " (count biosphere-users))
+    (log/debug "----------------------------------------------------------------")
+    (migrate-biosphere-users biosphere-users)))
