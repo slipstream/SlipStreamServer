4e2ba26661f0562bd710ee897c96bce5ecb8d817
Charles Loomis <cal@sixsq.com>
2018-07-25 08:06:25 +0200

    update parent

--- jar-placement-lib/project.clj
+++ jar-placement-lib/project.clj
@@ -12,7 +12,7 @@
 
   :plugins [[lein-parent "0.3.2"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.3.9"]
+  :parent-project {:coords  [sixsq/slipstream-parent "5.3.11"]
                    :inherit [:plugins
                              :min-lein-version
                              :managed-dependencies
