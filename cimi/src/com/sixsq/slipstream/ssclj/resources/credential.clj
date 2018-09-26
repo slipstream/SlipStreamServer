c8a49d0a720ab4e3b7985000a216fb37f4dc96f4
Khaled Basbous <khaled@sixsq.com>
2018-09-26 17:13:16 +0200

    api key bug fix (#1663)

--- cimi/src/com/sixsq/slipstream/ssclj/resources/credential.clj
+++ cimi/src/com/sixsq/slipstream/ssclj/resources/credential.clj
@@ -5,7 +5,8 @@
     [com.sixsq.slipstream.ssclj.resources.common.schema :as c]
     [com.sixsq.slipstream.ssclj.resources.common.std-crud :as std-crud]
     [com.sixsq.slipstream.ssclj.resources.common.utils :as u]
-    [com.sixsq.slipstream.ssclj.util.log :as logu]))
+    [com.sixsq.slipstream.ssclj.util.log :as logu]
+    [com.sixsq.slipstream.db.impl :as db]))
 
 (def ^:const resource-tag :credentials)
 
@@ -56,7 +57,8 @@
 
 (defmethod create-validate-subtype :default
   [resource]
-  (logu/log-and-throw-400 (str "cannot validate CredentialTemplate create document with type: '" (dispatch-on-registration-method resource) "'")))
+  (logu/log-and-throw-400 (str "cannot validate CredentialTemplate create document with type: '"
+                               (dispatch-on-registration-method resource) "'")))
 
 (defmethod crud/validate create-uri
   [resource]
@@ -94,7 +96,8 @@
 ;; default implementation throws if the credential type is unknown
 (defmethod tpl->credential :default
   [resource request]
-  (logu/log-and-throw-400 (str "cannot transform CredentialTemplate document to template for type: '" (:type resource) "'")))
+  (logu/log-and-throw-400
+    (str "cannot transform CredentialTemplate document to template for type: '" (:type resource) "'")))
 
 ;;
 ;; CRUD operations
@@ -108,17 +111,17 @@
 
 ;; Use standard method for setting operations.
 #_(defmethod crud/set-operations resource-uri
-  [resource request]
-  (try
-    (a/can-modify? resource request)
-    (let [href (:id resource)
-          ^String resourceURI (:resourceURI resource)
-          ops (if (.endsWith resourceURI "Collection")
-                [{:rel (:add c/action-uri) :href href}]
-                [{:rel (:delete c/action-uri) :href href}])]
-      (assoc resource :operations ops))
-    (catch Exception e
-      (dissoc resource :operations))))
+    [resource request]
+    (try
+      (a/can-modify? resource request)
+      (let [href (:id resource)
+            ^String resourceURI (:resourceURI resource)
+            ops (if (.endsWith resourceURI "Collection")
+                  [{:rel (:add c/action-uri) :href href}]
+                  [{:rel (:delete c/action-uri) :href href}])]
+        (assoc resource :operations ops))
+      (catch Exception e
+        (dissoc resource :operations))))
 
 (defn check-connector-exists
   "Use ADMIN role as we only want to check if href points to an existing
@@ -126,7 +129,7 @@
   [body idmap]
   (let [admin {:identity {:current         "internal",
                           :authentications {"internal" {:roles #{"ADMIN"}, :identity "internal"}}}}
-        href  (get-in body [:credentialTemplate :connector])]
+        href (get-in body [:credentialTemplate :connector])]
     (std-crud/resolve-hrefs href admin))
   body)
 
@@ -134,7 +137,7 @@
   [body idmap]
   (let [connector-href (if (contains? (:credentialTemplate body) :connector)
                          {:connector (get-in body [:credentialTemplate :connector])}
-                         {})] ;; to put back the unexpanded href after
+                         {})]                               ;; to put back the unexpanded href after
     (-> body
         (check-connector-exists idmap)
         ;; remove connector href (if any); regular user doesn't have rights to see them
@@ -146,25 +149,38 @@
 ;; requires a CredentialTemplate to create new Credential
 (defmethod crud/add resource-name
   [{:keys [body] :as request}]
-  (let [idmap      {:identity (:identity request)}
+  (let [idmap {:identity (:identity request)}
         desc-attrs (u/select-desc-keys body)
-        [create-resp {:keys [id] :as body}] (-> body
-                                                (assoc :resourceURI create-uri)
-                                                (update-in [:credentialTemplate] dissoc :type) ;; forces use of template reference
-                                                (resolve-hrefs idmap)
-                                                (update-in [:credentialTemplate] merge desc-attrs) ;; ensure desc attrs are validated
-                                                crud/validate
-                                                :credentialTemplate
-                                                (tpl->credential request))]
+        [create-resp {:keys [id] :as body}]
+        (-> body
+            (assoc :resourceURI create-uri)
+            (update-in [:credentialTemplate] dissoc :type)  ;; forces use of template reference
+            (resolve-hrefs idmap)
+            (update-in [:credentialTemplate] merge desc-attrs) ;; ensure desc attrs are validated
+            crud/validate
+            :credentialTemplate
+            (tpl->credential request))]
     (-> request
         (assoc :id id :body (merge body desc-attrs))
         add-impl
         (update-in [:body] merge create-resp))))
 
+(defmulti special-edit dispatch-conversion)
+
+(defmethod special-edit :default
+  [resource _]
+  resource)
+
 (def edit-impl (std-crud/edit-fn resource-name))
 (defmethod crud/edit resource-name
-  [request]
-  (edit-impl request))
+  [{{uuid :uuid} :params body :body :as request}]
+  (let [type (-> (str (u/de-camelcase resource-name) "/" uuid)
+                 (db/retrieve request)
+                 :type)
+        new-body (-> body
+                     (assoc :type type)
+                     (special-edit request))]
+    (edit-impl (assoc request :body new-body))))
 
 (def retrieve-impl (std-crud/retrieve-fn resource-name))
 (defmethod crud/retrieve resource-name
