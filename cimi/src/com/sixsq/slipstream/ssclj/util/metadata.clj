30be00b573b68701c1aa3c5adfd2dd7aab4bd5ad
Charles Loomis <cal@sixsq.com>
2018-10-29 14:10:10 +0100

    update resource metadata utility

--- cimi/src/com/sixsq/slipstream/ssclj/util/metadata.clj
+++ cimi/src/com/sixsq/slipstream/ssclj/util/metadata.clj
@@ -1,6 +1,10 @@
 (ns com.sixsq.slipstream.ssclj.util.metadata
   (:require
-    [spec-tools.json-schema :as jsc]))
+    [spec-tools.json-schema :as jsc]
+    [clojure.tools.logging :as log]
+    [com.sixsq.slipstream.ssclj.resources.resource-metadata :as resource-metadata])
+  (:import
+    (clojure.lang Namespace)))
 
 
 (defn strip-for-attributes
@@ -19,17 +23,11 @@
     [(keyword attribute-name) value-scope]))
 
 
-(defn generate
-  "generate the ResourceMetadata from the given spec"
-  [typeURI spec]
-  (let [common {:id          "resource-metadata/dummy-id"
-                :created     "1964-08-25T10:00:00.0Z"
-                :updated     "1964-08-25T10:00:00.0Z"
-                :resourceURI "https://example.org/ResourceMetadata"
-                :acl         {:owner {:principal "ADMIN", :type "ROLE"}}
-                :typeURI     typeURI}
-
-        json (jsc/transform spec)
+(defn generate-attributes
+  "generate the attributes and vscope fields of the resource metadata from the
+   schema definition"
+  [spec]
+  (let [json (jsc/transform spec)
 
         attributes (->> json
                         :properties
@@ -43,6 +41,91 @@
                     (remove nil?)
                     (into {}))]
 
-    (cond-> common
+    (cond-> {}
             (seq attributes) (assoc :attributes attributes)
             (seq vscope) (assoc :vscope vscope))))
+
+
+(defn get-doc
+  [resource-ns]
+  (-> resource-ns meta :doc))
+
+
+(defn get-resource-name
+  [resource-ns]
+  (some-> resource-ns
+          (ns-resolve 'resource-name)
+          deref))
+
+
+(defn get-type-uri
+  [resource-ns]
+  (some-> resource-ns
+          (ns-resolve 'resource-uri)
+          deref))
+
+
+(defn get-actions
+  [resource-ns]
+  (some-> resource-ns
+          (ns-resolve 'actions)
+          deref))
+
+
+(defn get-capabilities
+  [resource-ns]
+  (some-> resource-ns
+          (ns-resolve 'capabilities)
+          deref))
+
+
+(defn get-spec-kw
+  [resource-url]
+  (when resource-url
+    (keyword (str "com.sixsq.slipstream.ssclj.resources.spec." resource-url) resource-url)))
+
+
+(defn get-spec
+  [resource-ns]
+  (some-> resource-ns
+          (ns-resolve 'resource-url)
+          deref
+          get-spec-kw))
+
+
+(defn generate-from-ns
+  "generate the ResourceMetadata from the current (or provided) namespace"
+  ([]
+   (generate-from-ns *ns*))
+  ([resource-ns]
+   (if-let [resource-ns (if (instance? Namespace resource-ns)
+                          resource-ns
+                          (find-ns (symbol resource-ns)))]
+     (let [resource-name (get-resource-name resource-ns)
+           doc (get-doc resource-ns)
+           spec (get-spec resource-ns)
+           type-uri (get-type-uri resource-ns)
+
+           common {:id          "resource-metadata/dummy-id"
+                   :created     "1964-08-25T10:00:00.0Z"
+                   :updated     "1964-08-25T10:00:00.0Z"
+                   :resourceURI resource-metadata/resource-uri
+                   :acl         {:owner {:principal "ADMIN", :type "ROLE"}
+                                 :rules [{:principal "ANON", :type "ROLE", :right "VIEW"}]}
+                   :typeURI     type-uri
+                   :name        resource-name
+                   :description doc}
+
+           attributes (generate-attributes spec)
+
+           actions (get-actions resource-ns)
+
+           capabilities (get-capabilities resource-ns)]
+
+       (if (and doc spec type-uri)
+         (cond-> common
+                 attributes (merge attributes)
+                 (seq actions) (assoc :actions actions)
+                 (seq capabilities) (assoc :capabilities capabilities))
+         (log/error "namespace documentation, spec, and resource-uri cannot be null")))
+     (log/error "cannot find namespace for " (str resource-ns)))))
